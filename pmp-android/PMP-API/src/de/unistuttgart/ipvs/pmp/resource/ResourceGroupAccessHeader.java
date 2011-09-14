e16abcc669d04f2b3be44aa34cbbe5d7b4a2e0f5
Tobias Kuhn <kuhnts@studi.informatik.uni-stuttgart.de>
2011-09-14 18:57:01 +0200

    removed unnecessary equals() code, could cause problems

--- pmp-android/PMP-API/src/de/unistuttgart/ipvs/pmp/resource/ResourceGroupAccessHeader.java
+++ pmp-android/PMP-API/src/de/unistuttgart/ipvs/pmp/resource/ResourceGroupAccessHeader.java
@@ -35,16 +35,6 @@ public class ResourceGroupAccessHeader implements Parcelable {
     }
 
     @Override
-    public boolean equals(Object o) {
-	if (o instanceof ResourceGroupAccessHeader) {
-	    ResourceGroupAccessHeader rgah = (ResourceGroupAccessHeader) o;
-	    return (this.identifier.equals(rgah) && this.publicKey
-		    .equals(rgah.publicKey));
-	}
-	return false;
-    }
-
-    @Override
     public int describeContents() {
 	return this.identifier.hashCode() + this.publicKey.hashCode();
     }
