8061c788af98952ed3ad811383772d363e5fe2da
Jakob Jarosch <jakob.jarosch@gmail.com>
2011-09-14 22:19:52 +0200

    added a method to ServiceLevelPublisher refs #71

--- pmp-android/PMP/src/de/unistuttgart/ipvs/pmp/model/implementations/utils/ServiceLevelPublisher.java
+++ pmp-android/PMP/src/de/unistuttgart/ipvs/pmp/model/implementations/utils/ServiceLevelPublisher.java
@@ -6,9 +6,27 @@ public class ServiceLevelPublisher {
 
     private IApp app;
     private boolean asynchronous;
-    
+
     public ServiceLevelPublisher(IApp app, boolean asynchronous) {
 	this.app = app;
 	this.asynchronous = asynchronous;
+    }
+
+    public void publish() {
+	if (asynchronous) {
+	    new Thread(new Runnable() {
+
+		@Override
+		public void run() {
+		    executePublishing();
+		}
+	    }).start();
+	} else {
+	    executePublishing();
+	}
+    }
+    
+    private void executePublishing() {
+	
     }
 }
