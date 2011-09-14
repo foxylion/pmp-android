1f4edbef53782c85b9a64b3402a9cd459565beac
Patrick Strobel <patrick.strobel.swt@gmail.com>
2011-09-14 23:50:17 +0200

    Created project for file-system-resource and added some classes. Refs #90

---
+++ pmp-android/Resources/FileSystemResources/src/de/unistuttgart/ipvs/pmp/resources/FileSystemService.java
@@ -0,0 +1,7 @@
+package de.unistuttgart.ipvs.pmp.resources;
+
+import de.unistuttgart.ipvs.pmp.service.resource.ResourceGroupService;
+
+public class FileSystemService extends ResourceGroupService {
+
+}
