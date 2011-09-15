30f7bc0bc03d0374dfa7e40c1f41e99ce21b5cba
Tobias Kuhn <kuhnts@studi.informatik.uni-stuttgart.de>
2011-09-15 21:33:25 +0200

    some minor changes due to debugging

--- pmp-android/PMP-API/src/de/unistuttgart/ipvs/pmp/resource/ResourceGroup.java
+++ pmp-android/PMP-API/src/de/unistuttgart/ipvs/pmp/resource/ResourceGroup.java
@@ -29,9 +29,8 @@ import de.unistuttgart.ipvs.pmp.service.utils.PMPSignee;
  * 
  * <p>
  * In order to work, a ResourceGroup needs a service defined in the manifest
- * file which simply is {@link ResourceGroupService}, and the app
- * containing the ResourceGroup and its service must extend
- * {@link ResourceGroupApp}.
+ * file which simply is {@link ResourceGroupService}, and the app containing the
+ * ResourceGroup and its service must extend {@link ResourceGroupApp}.
  * </p>
  * 
  * @author Tobias Kuhn
@@ -39,234 +38,234 @@ import de.unistuttgart.ipvs.pmp.service.utils.PMPSignee;
  */
 public abstract class ResourceGroup {
 
-    /**
-     * Stores the associated signee.
-     */
-    private PMPSignee signee;
+	/**
+	 * Stores the associated signee.
+	 */
+	private PMPSignee signee;
 
-    /**
-     * The resources present in that resource group.
-     */
-    private final Map<String, Resource> resources;
+	/**
+	 * The resources present in that resource group.
+	 */
+	private final Map<String, Resource> resources;
 
-    /**
-     * The privacy levels present in that resource group.
-     */
-    private final Map<String, PrivacyLevel> privacyLevels;
+	/**
+	 * The privacy levels present in that resource group.
+	 */
+	private final Map<String, PrivacyLevel> privacyLevels;
 
-    /**
-     * Stores the list of the privacy level values.
-     */
-    private final Map<String, Bundle> privacyLevelValues;
+	/**
+	 * Stores the list of the privacy level values.
+	 */
+	private final Map<String, Bundle> privacyLevelValues;
 
-    /**
-     * Creates a new {@link ResourceGroup}.
-     * 
-     * @param serviceContext
-     *            context of the service for this resource group
-     */
-    public ResourceGroup(Context serviceContext) {
-	signee = new PMPSignee(PMPComponentType.RESOURCE_GROUP,
-		getServiceAndroidName(), serviceContext);
+	/**
+	 * Creates a new {@link ResourceGroup}.
+	 * 
+	 * @param serviceContext
+	 *            context of the service for this resource group
+	 */
+	public ResourceGroup(Context serviceContext) {
+		signee = new PMPSignee(PMPComponentType.RESOURCE_GROUP,
+				getServiceAndroidName(), serviceContext);
 
-	resources = new HashMap<String, Resource>();
-	privacyLevels = new HashMap<String, PrivacyLevel>();
-	privacyLevelValues = new HashMap<String, Bundle>();
-    }
-
-    /**
-     * 
-     * @param locale
-     *            the ISO-639 locale string available from
-     *            {@link Locale#getLanguage()}
-     * @return the name of this resource group for the given locale
-     */
-    public abstract String getName(String locale);
-
-    /**
-     * 
-     * @param locale
-     *            the ISO-639 locale string available from
-     *            {@link Locale#getLanguage()}
-     * @return the description of this resource group for the given locale
-     */
-    public abstract String getDescription(String locale);
-
-    /**
-     * Overwrite this method to return the <b>exact same</b> identifier you have
-     * put in the manifest file for the service for this Resource Group:
-     * &lt;service>...&lt;intent-filter>...&lt;action
-     * android:name="<b>HERE</b>">. If the identifier differ, the service will
-     * not work.
-     * 
-     * @return the specified identifier
-     */
-    protected abstract String getServiceAndroidName();
-
-    /**
-     * 
-     * @return the signee
-     */
-    public PMPSignee getSignee() {
-	return this.signee;
-    }
-
-    /**
-     * Registers resource as resource "identifier" in this resource group.
-     * 
-     * @param identifier
-     * @param resource
-     */
-    public void registerResource(String identifier, Resource resource) {
-	resource.assignResourceGroup(this);
-	resources.put(identifier, resource);
-    }
-
-    /**
-     * 
-     * @param identifier
-     * @return the resource identified by "identifier", if present, null
-     *         otherwise
-     */
-    public Resource getResource(String identifier) {
-	return resources.get(identifier);
-    }
-
-    /**
-     * 
-     * @return a list of all the valid resource identifiers.
-     */
-    public List<String> getResources() {
-	return new ArrayList<String>(resources.keySet());
-    }
-
-    /**
-     * Registers privacyLevel as privacy level "identifier" in this resource
-     * group.
-     * 
-     * @param identifier
-     * @param privacyLevel
-     */
-    public void registerPrivacyLevel(String identifier,
-	    PrivacyLevel privacyLevel) {
-	privacyLevels.put(identifier, privacyLevel);
-    }
-
-    /**
-     * 
-     * @param identifier
-     * @return the privacy level identified by "identifier", if present, null
-     *         otherwise
-     */
-    public PrivacyLevel getPrivacyLevel(String identifier) {
-	return privacyLevels.get(identifier);
-    }
-
-    /**
-     * 
-     * @return a list of all the valid resource identifiers.
-     */
-    public List<String> getPrivacyLevels() {
-	return new ArrayList<String>(privacyLevels.keySet());
-    }
-
-    /**
-     * Used for getting changed access rules from the
-     * {@link ResourceGroupService}. <b>Do not call this method yourself or
-     * allow a different context to call it.</b>
-     * 
-     * @param rga
-     *            the {@link ResourceGroupAccess} to update
-     */
-    public final void updateAccess(ResourceGroupAccess rga) {
-	privacyLevelValues.put(rga.getHeader().getIdentifier(),
-		rga.getPrivacyLevelValues());
-    }
-
-    /**
-     * 
-     * @param appIdentifier
-     *            the identifier of the accessing app
-     * @param privacyLevel
-     *            the identifier of the privacy level
-     * @return the privacy level value identified by "privacyLevel" for the app
-     *         "appIdentifier", if present, null otherwise
-     */
-    protected final String getPrivacyLevelValue(String appIdentifier,
-	    String privacyLevel) {
-	Bundle appPLs = privacyLevelValues.get(appIdentifier);
-	if (appPLs == null) {
-	    return null;
-	} else {
-	    return appPLs.getString(privacyLevel);
+		resources = new HashMap<String, Resource>();
+		privacyLevels = new HashMap<String, PrivacyLevel>();
+		privacyLevelValues = new HashMap<String, Bundle>();
 	}
-    }
 
-    /**
-     * Effectively starts this resource group and registers it with PMP. Note
-     * that it needs to be "connected" to a {@link ResourceGroupService} via a
-     * {@link ResourceGroupApp}. You can implement reacting to the result of
-     * this operation by implementing onRegistrationSuccess() or
-     * onRegistrationFailed()
-     * 
-     * @param context
-     *            {@link Context} to use for the connection
-     * 
-     */
-    public void start(Context context) {
+	/**
+	 * 
+	 * @param locale
+	 *            the ISO-639 locale string available from
+	 *            {@link Locale#getLanguage()}
+	 * @return the name of this resource group for the given locale
+	 */
+	public abstract String getName(String locale);
 
-	// connect to PMP
-	final PMPServiceConnector pmpsc = new PMPServiceConnector(context,
-		signee);
+	/**
+	 * 
+	 * @param locale
+	 *            the ISO-639 locale string available from
+	 *            {@link Locale#getLanguage()}
+	 * @return the description of this resource group for the given locale
+	 */
+	public abstract String getDescription(String locale);
 
-	pmpsc.addCallbackHandler(new IConnectorCallback() {
+	/**
+	 * Overwrite this method to return the <b>exact same</b> identifier you have
+	 * put in the manifest file for the service for this Resource Group:
+	 * &lt;service>...&lt;intent-filter>...&lt;action
+	 * android:name="<b>HERE</b>">. If the identifier differ, the service will
+	 * not work.
+	 * 
+	 * @return the specified identifier
+	 */
+	protected abstract String getServiceAndroidName();
 
-	    @Override
-	    public void disconnected() {
-	    }
+	/**
+	 * 
+	 * @return the signee
+	 */
+	public PMPSignee getSignee() {
+		return this.signee;
+	}
 
-	    @Override
-	    public void connected() {
-		if (!pmpsc.isRegistered()) {
-		    // register with PMP
-		    IPMPServiceRegistration ipmpsr = pmpsc
-			    .getRegistrationService();
-		    try {
-			byte[] pmpPublicKey = ipmpsr
-				.registerResourceGroup(signee
-					.getLocalPublicKey());
+	/**
+	 * Registers resource as resource "identifier" in this resource group.
+	 * 
+	 * @param identifier
+	 * @param resource
+	 */
+	public void registerResource(String identifier, Resource resource) {
+		resource.assignResourceGroup(this);
+		resources.put(identifier, resource);
+	}
 
-			// save the returned public key to be PMP's
-			signee.setRemotePublicKey(PMPComponentType.PMP,
-				Constants.PMP_IDENTIFIER, pmpPublicKey);
+	/**
+	 * 
+	 * @param identifier
+	 * @return the resource identified by "identifier", if present, null
+	 *         otherwise
+	 */
+	public Resource getResource(String identifier) {
+		return resources.get(identifier);
+	}
 
-		    } catch (RemoteException e) {
-			Log.e("RemoteException during registering resource group",
-				e);
-		    }
+	/**
+	 * 
+	 * @return a list of all the valid resource identifiers.
+	 */
+	public List<String> getResources() {
+		return new ArrayList<String>(resources.keySet());
+	}
+
+	/**
+	 * Registers privacyLevel as privacy level "identifier" in this resource
+	 * group.
+	 * 
+	 * @param identifier
+	 * @param privacyLevel
+	 */
+	public void registerPrivacyLevel(String identifier,
+			PrivacyLevel privacyLevel) {
+		privacyLevels.put(identifier, privacyLevel);
+	}
+
+	/**
+	 * 
+	 * @param identifier
+	 * @return the privacy level identified by "identifier", if present, null
+	 *         otherwise
+	 */
+	public PrivacyLevel getPrivacyLevel(String identifier) {
+		return privacyLevels.get(identifier);
+	}
+
+	/**
+	 * 
+	 * @return a list of all the valid resource identifiers.
+	 */
+	public List<String> getPrivacyLevels() {
+		return new ArrayList<String>(privacyLevels.keySet());
+	}
+
+	/**
+	 * Used for getting changed access rules from the
+	 * {@link ResourceGroupService}. <b>Do not call this method yourself or
+	 * allow a different context to call it.</b>
+	 * 
+	 * @param rga
+	 *            the {@link ResourceGroupAccess} to update
+	 */
+	public final void updateAccess(ResourceGroupAccess rga) {
+		privacyLevelValues.put(rga.getHeader().getIdentifier(),
+				rga.getPrivacyLevelValues());
+	}
+
+	/**
+	 * 
+	 * @param appIdentifier
+	 *            the identifier of the accessing app
+	 * @param privacyLevel
+	 *            the identifier of the privacy level
+	 * @return the privacy level value identified by "privacyLevel" for the app
+	 *         "appIdentifier", if present, null otherwise
+	 */
+	protected final String getPrivacyLevelValue(String appIdentifier,
+			String privacyLevel) {
+		Bundle appPLs = privacyLevelValues.get(appIdentifier);
+		if (appPLs == null) {
+			return null;
+		} else {
+			return appPLs.getString(privacyLevel);
 		}
-	    }
+	}
 
-	    @Override
-	    public void bindingFailed() {
-		Log.e("Binding failed during registering resource group.");
-	    }
-	});
-	pmpsc.bind();
-    }
+	/**
+	 * Effectively starts this resource group and registers it with PMP. Note
+	 * that it needs to be "connected" to a {@link ResourceGroupService} via a
+	 * {@link ResourceGroupApp}. You can implement reacting to the result of
+	 * this operation by implementing onRegistrationSuccess() or
+	 * onRegistrationFailed()
+	 * 
+	 * @param context
+	 *            {@link Context} to use for the connection
+	 * 
+	 */
+	public void start(Context context) {
 
-    /**
-     * Callback called when the preceding call to start() registered this
-     * resource group successfully with PMP.
-     */
-    public abstract void onRegistrationSuccess();
+		// connect to PMP
+		final PMPServiceConnector pmpsc = new PMPServiceConnector(context,
+				signee);
 
-    /**
-     * Callback called when the preceding call to start() could not register
-     * this resource group with PMP due to errors.
-     * 
-     * @param message
-     *            returned message from the PMP service
-     */
-    public abstract void onRegistrationFailed(String message);
+		pmpsc.addCallbackHandler(new IConnectorCallback() {
+
+			@Override
+			public void disconnected() {
+			}
+
+			@Override
+			public void connected() {
+				if (!pmpsc.isRegistered()) {
+					// register with PMP
+					IPMPServiceRegistration ipmpsr = pmpsc
+							.getRegistrationService();
+					try {
+						byte[] pmpPublicKey = ipmpsr
+								.registerResourceGroup(signee
+										.getLocalPublicKey());
+
+						// save the returned public key to be PMP's
+						signee.setRemotePublicKey(PMPComponentType.PMP,
+								Constants.PMP_IDENTIFIER, pmpPublicKey);
+
+					} catch (RemoteException e) {
+						Log.e("RemoteException during registering resource group",
+								e);
+					}
+				}
+			}
+
+			@Override
+			public void bindingFailed() {
+				Log.e("Binding failed during registering resource group.");
+			}
+		});
+		pmpsc.bind();
+	}
+
+	/**
+	 * Callback called when the preceding call to start() registered this
+	 * resource group successfully with PMP.
+	 */
+	public abstract void onRegistrationSuccess();
+
+	/**
+	 * Callback called when the preceding call to start() could not register
+	 * this resource group with PMP due to errors.
+	 * 
+	 * @param message
+	 *            returned message from the PMP service
+	 */
+	public abstract void onRegistrationFailed(String message);
 }
--- pmp-android/Resources/DatabaseResourceGroup/src/de/unistuttgart/ipvs/pmp/resourcegroups/database/DatabaseResourceGroup.java
+++ pmp-android/Resources/DatabaseResourceGroup/src/de/unistuttgart/ipvs/pmp/resourcegroups/database/DatabaseResourceGroup.java
@@ -16,99 +16,98 @@ import de.unistuttgart.ipvs.pmp.service.PMPSignedService;
  */
 public class DatabaseResourceGroup extends ResourceGroup {
 
-    public static final String DATABASE_RESOURCE_IDENTIFIER = 
-	    "de.unistuttgart.ipvs.pmp.resourcegroups.IDatabaseAccess";
-    public static final String PRIVACY_LEVEL_READ = "read";
-    public static final String PRIVACY_LEVEL_WRITE = "write";
-    public static final String PRIVACY_LEVEL_DELETE = "delete";
-    // public static final String PRIVACY_LEVEL_CREATE = "create";
+	public static final String DATABASE_RESOURCE_IDENTIFIER = "de.unistuttgart.ipvs.pmp.resourcegroups.database";
+	public static final String PRIVACY_LEVEL_READ = "read";
+	public static final String PRIVACY_LEVEL_WRITE = "write";
+	public static final String PRIVACY_LEVEL_DELETE = "delete";
+	// public static final String PRIVACY_LEVEL_CREATE = "create";
 
-    private DatabaseResource dbr;
-    private Class<? extends PMPSignedService> service;
-    private Context context;
+	private DatabaseResource dbr;
+	private Class<? extends PMPSignedService> service;
+	private Context context;
 
-    // PL
-    private SimplePrivacyLevel read, write, delete;
+	// PL
+	private SimplePrivacyLevel read, write, delete;
 
-    public DatabaseResourceGroup(Context serviceContext,
-	    Class<? extends PMPSignedService> service) {
-	super(serviceContext);
-	context = serviceContext;
-	this.service = service;
-	Log.d(context.getResources().getString(R.string.resource_group_name));
+	public DatabaseResourceGroup(Context serviceContext,
+			Class<? extends PMPSignedService> service) {
+		super(serviceContext);
+		context = serviceContext;
+		this.service = service;
+		Log.d(context.getResources().getString(R.string.resource_group_name));
 
-	// Register the privacy levels
-	// TODO: Create the privacy levels
-	registerPrivacyLevel(PRIVACY_LEVEL_READ, read);
-	registerPrivacyLevel(PRIVACY_LEVEL_WRITE, write);
-	registerPrivacyLevel(PRIVACY_LEVEL_DELETE, delete);
+		// Register the privacy levels
+		// TODO: Create the privacy levels
+		registerPrivacyLevel(PRIVACY_LEVEL_READ, read);
+		registerPrivacyLevel(PRIVACY_LEVEL_WRITE, write);
+		registerPrivacyLevel(PRIVACY_LEVEL_DELETE, delete);
 
-	// Register the resource
-	// TODO: Where should the resource be created? Only when an authorized
-	// application request, right?
-	registerResource(DATABASE_RESOURCE_IDENTIFIER, dbr);
-    }
+		// Register the resource
+		// TODO: Where should the resource be created? Only when an authorized
+		// application request, right?
+		dbr = new DatabaseResource();
+		registerResource(DATABASE_RESOURCE_IDENTIFIER, dbr);
+	}
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see
-     * de.unistuttgart.ipvs.pmp.resource.ResourceGroup#getName(java.lang.String)
-     */
-    @Override
-    public String getName(String locale) {
-	// TODO: Locale or not locale?
-	return context.getResources().getString(R.string.resource_group_name);
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * de.unistuttgart.ipvs.pmp.resource.ResourceGroup#getName(java.lang.String)
+	 */
+	@Override
+	public String getName(String locale) {
+		// TODO: Locale or not locale?
+		return context.getResources().getString(R.string.resource_group_name);
+	}
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see
-     * de.unistuttgart.ipvs.pmp.resource.ResourceGroup#getDescription(java.lang
-     * .String)
-     */
-    @Override
-    public String getDescription(String locale) {
-	// TODO: Locale or not locale?
-	return context.getResources().getString(R.string.resource_group_name);
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * de.unistuttgart.ipvs.pmp.resource.ResourceGroup#getDescription(java.lang
+	 * .String)
+	 */
+	@Override
+	public String getDescription(String locale) {
+		// TODO: Locale or not locale?
+		return context.getResources().getString(R.string.resource_group_name);
+	}
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see
-     * de.unistuttgart.ipvs.pmp.resource.ResourceGroup#getServiceAndroidName()
-     */
-    @Override
-    protected String getServiceAndroidName() {
-	// TODO Is this right?
-	return service.toString();
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * de.unistuttgart.ipvs.pmp.resource.ResourceGroup#getServiceAndroidName()
+	 */
+	@Override
+	protected String getServiceAndroidName() {
+		return DATABASE_RESOURCE_IDENTIFIER;
+	}
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see
-     * de.unistuttgart.ipvs.pmp.resource.ResourceGroup#onRegistrationSuccess()
-     */
-    @Override
-    public void onRegistrationSuccess() {
-	// TODO Auto-generated method stub
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * de.unistuttgart.ipvs.pmp.resource.ResourceGroup#onRegistrationSuccess()
+	 */
+	@Override
+	public void onRegistrationSuccess() {
+		// TODO Auto-generated method stub
 
-    }
+	}
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see
-     * de.unistuttgart.ipvs.pmp.resource.ResourceGroup#onRegistrationFailed(
-     * java.lang.String)
-     */
-    @Override
-    public void onRegistrationFailed(String message) {
-	// TODO Auto-generated method stub
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * de.unistuttgart.ipvs.pmp.resource.ResourceGroup#onRegistrationFailed(
+	 * java.lang.String)
+	 */
+	@Override
+	public void onRegistrationFailed(String message) {
+		// TODO Auto-generated method stub
 
-    }
+	}
 
 }
