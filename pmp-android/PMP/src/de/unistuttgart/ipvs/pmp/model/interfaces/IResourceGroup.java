495d51d78e51eab9ff374399fbde5ae8af37e814
Jakob Jarosch <jakob.jarosch@gmail.com>
2011-09-16 21:39:42 +0200

    changed JavaDoc-comments for IResourceGroup

--- pmp-android/PMP/src/de/unistuttgart/ipvs/pmp/model/interfaces/IResourceGroup.java
+++ pmp-android/PMP/src/de/unistuttgart/ipvs/pmp/model/interfaces/IResourceGroup.java
@@ -13,22 +13,30 @@ public interface IResourceGroup {
     public String getIdentifier();
 
     /**
-     * @return Returns the localized name of the resource group.
+     * @return Returns the localized name of the {@link IResourceGroup}.
      */
     public String getName();
 
     /**
-     * @return Returns the localized description of the resource group.
+     * @return Returns the localized description of the {@link IResourceGroup}.
      */
     public String getDescription();
 
     /**
-     * @return Returns all usable {@link IPrivacyLevel}s of the resource group.
+     * @return Returns all usable {@link IPrivacyLevel}s of the
+     *         {@link IResourceGroup}.
      */
     public IPrivacyLevel[] getPrivacyLevels();
 
     /**
-     * @return Returns all {@link IApp} which are <b>currently</b> using this {@link IResourceGroup}.
+     * This method return all {@link IApp}s which are currently using the
+     * {@link IResourceGroup}.<br>
+     * Currently means that the {@link IApp} has an {@link IServiceLevel} set as
+     * active where one of the {@link IResourceGroup}s {@link IPrivacyLevel} is
+     * referenced.
+     * 
+     * @return Returns all {@link IApp} which are <b>currently</b> using this
+     *         {@link IResourceGroup}.
      */
     IApp[] getAllAppsUsingThisResourceGroup();
 }
