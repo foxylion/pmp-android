8026b87cf15e5fe7ab2d468184f1af5818bcbb8e
Jakob Jarosch <jakob.jarosch@gmail.com>
2011-09-16 21:45:43 +0200

    changed JavaDoc-comments for IServiceLevel

--- pmp-android/PMP/src/de/unistuttgart/ipvs/pmp/model/interfaces/IServiceLevel.java
+++ pmp-android/PMP/src/de/unistuttgart/ipvs/pmp/model/interfaces/IServiceLevel.java
@@ -1,35 +1,47 @@
 package de.unistuttgart.ipvs.pmp.model.interfaces;
 
 /**
- * {@link IServiceLevel} is served by a {@link IApp}, containing all required
- * {@link IPrivacyLevel}s for an accordingly service of the .
+ * {@link IServiceLevel} is returned by an {@link IApp}, containing all required
+ * {@link IPrivacyLevel}s for an accordingly service of the {@link IApp}.
+ * 
+ * <p>
+ * It is possible that an {@link IServiceLevel} is not available (can be checked
+ * using {@link IServiceLevel#isAvailable()}), that means the
+ * {@link IServiceLevel} is using a {@link IPrivacyLevel} of an
+ * {@link IResourceGroup} which are not registered at PMP or does not have this
+ * {@link IPrivacyLevel} as a choice. They will be made available when
+ * {@link IResourceGroup} is registered at PMP and does offer all the required
+ * {@link IPrivacyLevel}s.
  * 
  * @author Jakob Jarosch
  */
 public interface IServiceLevel {
 
     /**
-     * @return returns the level position of the service level.
+     * @return returns the unique level of the {@link IServiceLevel}.
      */
     public int getLevel();
 
     /**
-     * @return Returns the localized name of the service level.
+     * @return Returns the localized name of the {@link IServiceLevel}.
      */
     public String getName();
 
     /**
-     * @return Returns the localized description of the service level.
+     * @return Returns the localized description of the {@link IServiceLevel}.
      */
     public String getDescription();
 
     /**
-     * @return Returns the required privacy levels for the service level.
+     * @return Returns the required {@link IPrivacyLevel}s for the
+     *         {@link IServiceLevel}.
      */
     public IPrivacyLevel[] getPrivacyLevels();
 
     /**
-     * @return Returns if a {@link IServiceLevel} is available for setting or not.
+     * @return Returns if a {@link IServiceLevel} is available for setting or
+     *         not. (more details can be found in JavaDoc of
+     *         {@link IServiceLevel}).
      */
     public boolean isAvailable();
 }
