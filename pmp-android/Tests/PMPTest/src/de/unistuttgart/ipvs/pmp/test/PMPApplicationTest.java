e63a7105eccc84bd5c4ea7f4986fed03622bab02
andre <nguyen.andres@gmail.com>
2011-09-17 13:23:35 +0200

    created test folder for CalendarApp, PMP, PMP-API

---
+++ pmp-android/Tests/PMPTest/src/de/unistuttgart/ipvs/pmp/test/PMPApplicationTest.java
@@ -0,0 +1,13 @@
+package de.unistuttgart.ipvs.pmp.test;
+
+import de.unistuttgart.ipvs.pmp.PMPApplication;
+import android.test.ApplicationTestCase;
+
+public class PMPApplicationTest extends
+		ApplicationTestCase<PMPApplication> {
+
+	public PMPApplicationTest() {
+		super(PMPApplication.class);
+	}
+	
+}
