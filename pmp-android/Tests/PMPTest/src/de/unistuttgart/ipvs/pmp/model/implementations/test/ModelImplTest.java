27b759cb2ccfd766c83eb6b50723ce54e86e6cc6
Alexander Wassiljew <alexanderwassiljew@googlemail.com>
2011-09-19 15:36:15 +0200

    Test cases for Model refs 130

--- pmp-android/Tests/PMPTest/src/de/unistuttgart/ipvs/pmp/model/implementations/test/ModelImplTest.java
+++ pmp-android/Tests/PMPTest/src/de/unistuttgart/ipvs/pmp/model/implementations/test/ModelImplTest.java
@@ -1,10 +1,14 @@
-package de.unistuttgart.ipvs.pmp.model.implementation.test;
+package de.unistuttgart.ipvs.pmp.model.implementations.test;
 
 import junit.framework.TestCase;
 import de.unistuttgart.ipvs.pmp.model.ModelSingleton;
 import de.unistuttgart.ipvs.pmp.model.interfaces.IApp;
 import de.unistuttgart.ipvs.pmp.model.interfaces.IResourceGroup;
-
+/**
+ * Testing the Model
+ * @author Alexander Wassiljew
+ *
+ */
 public class ModelImplTest extends TestCase {
 
 	IApp apps[];
@@ -14,20 +18,45 @@ public class ModelImplTest extends TestCase {
 		super(name);
 		
 	}
-
+	/**
+	 * Set up the testing environment
+	 */
 	protected void setUp() throws Exception {
 		apps = ModelSingleton.getInstance().getModel().getApps();
 		ress = ModelSingleton.getInstance().getModel().getResourceGroups();
 		System.out.println(apps.length);
 	}
 	
+	/**
+	 * Testing the Preconditions
+	 */
 	public void testPreconditions(){
 		assertNotNull(apps);
 		assertNotNull(ress);
 	}
 	
-	public void testAppsLength(){
+	public void testLength(){
 		assertEquals(2, apps.length);
+		assertEquals(3, ress.length);
+	}
+
+	public void testApp1(){
+		testIdentifierApp1();
+		testNameApp1();
+	}
+	private void testNameApp1() {
+		// TODO Auto-generated method stub
+		
+	}
+	public void testApp2(){
+		testIdentifierApp2();
+	}
+	
+	public void testIdentifierApp1(){
+		assertEquals("Sample#pmp.test.app1", apps[0].getIdentifier());
+	}
+	public void testIdentifierApp2(){
+		assertEquals("Sample#pmp.test.app2", apps[1].getIdentifier());
 	}
 
 }
