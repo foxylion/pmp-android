c91c819968de9e15a67ee6d87be2f72b26354c12
Tobias Kuhn <kuhnts@studi.informatik.uni-stuttgart.de>
2011-09-20 16:26:09 +0200

    Another refactoring of ServiceLevelCalcTest

--- pmp-android/Tests/PMPTest/src/de/unistuttgart/ipvs/pmp/model/implementations/test/utils/ServiceLevelCalculatorTest/ServiceLevelCalculatorTest.java
+++ pmp-android/Tests/PMPTest/src/de/unistuttgart/ipvs/pmp/model/implementations/test/utils/ServiceLevelCalculatorTest/ServiceLevelCalculatorTest.java
@@ -1,13 +1,14 @@
 package de.unistuttgart.ipvs.pmp.model.implementations.test.utils.ServiceLevelCalculatorTest;
 
 import android.os.RemoteException;
-import de.unistuttgart.ipvs.pmp.model.implementations.test.utils.ServiceLevelCalculatorTest.TestPresets.TestPreset1;
-import de.unistuttgart.ipvs.pmp.model.implementations.test.utils.ServiceLevelCalculatorTest.TestPresets.TestPreset2;
-import de.unistuttgart.ipvs.pmp.model.implementations.test.utils.ServiceLevelCalculatorTest.TestPresets.TestPreset3;
-import de.unistuttgart.ipvs.pmp.model.implementations.test.utils.ServiceLevelCalculatorTest.TestPresets.TestPreset4;
-import de.unistuttgart.ipvs.pmp.model.implementations.test.utils.ServiceLevelCalculatorTest.TestPresets.TestPreset5;
+import de.unistuttgart.ipvs.pmp.model.implementations.test.utils.ServiceLevelCalculatorTest.TestAccuracyRessourceGroup.TestAccuracyPrivacyLevel;
+import de.unistuttgart.ipvs.pmp.model.implementations.test.utils.ServiceLevelCalculatorTest.TestLocationRessourceGroup.TestPrivacyLevelBadenWuerttemberg;
+import de.unistuttgart.ipvs.pmp.model.implementations.test.utils.ServiceLevelCalculatorTest.TestLocationRessourceGroup.TestPrivacyLevelDeutschland;
+import de.unistuttgart.ipvs.pmp.model.implementations.test.utils.ServiceLevelCalculatorTest.TestLocationRessourceGroup.TestPrivacyLevelStuttgart;
+import de.unistuttgart.ipvs.pmp.model.implementations.test.utils.ServiceLevelCalculatorTest.TestPresets.TestPreset;
 import de.unistuttgart.ipvs.pmp.model.implementations.utils.ServiceLevelCalculator;
 import de.unistuttgart.ipvs.pmp.model.interfaces.IPreset;
+import de.unistuttgart.ipvs.pmp.model.interfaces.IPrivacyLevel;
 import junit.framework.TestCase;
 
 public class ServiceLevelCalculatorTest extends TestCase {
@@ -16,6 +17,19 @@ public class ServiceLevelCalculatorTest extends TestCase {
 	private IPreset[] presets1;
 	private IPreset[] presets2;
 	private IPreset[] presets3;
+
+	private final static IPreset TEST_PRESET_1 = new TestPreset(
+			new IPrivacyLevel[] { new TestAccuracyPrivacyLevel("1") });
+	private final static IPreset TEST_PRESET_2 = new TestPreset(
+			new IPrivacyLevel[] { new TestAccuracyPrivacyLevel("50") });
+	private final static IPreset TEST_PRESET_3 = new TestPreset(
+			new IPrivacyLevel[] { new TestAccuracyPrivacyLevel("50"),
+					new TestPrivacyLevelStuttgart() });
+	private final static IPreset TEST_PRESET_4 = new TestPreset(
+			new IPrivacyLevel[] { new TestAccuracyPrivacyLevel("600"),
+					new TestPrivacyLevelBadenWuerttemberg() });
+	private final static IPreset TEST_PRESET_5 = new TestPreset(
+			new IPrivacyLevel[] { new TestPrivacyLevelDeutschland() });
 
 	/**
 	 * Set up the testing environment
@@ -29,17 +43,17 @@ public class ServiceLevelCalculatorTest extends TestCase {
 
 	public void testCalculate1() {
 		// Test 1: Preset 1 -> SL 0
-		presets1[0] = new TestPreset1();
+		presets1[0] = TEST_PRESET_1;
 		testApp.setPresets(presets1);
 		try {
 			assertEquals(0, new ServiceLevelCalculator(testApp).calculate());
 		} catch (RemoteException ignore) {
 		}
 	}
-	
+
 	public void testCalculate2() {
 		// Test 2: Preset 2 -> SL 1
-		presets1[0] = new TestPreset2();
+		presets1[0] = TEST_PRESET_2;
 		testApp.setPresets(presets1);
 		try {
 			assertEquals(1, new ServiceLevelCalculator(testApp).calculate());
@@ -49,7 +63,7 @@ public class ServiceLevelCalculatorTest extends TestCase {
 
 	public void testCalculate3() {
 		// Test 3: Preset 3 -> SL 0
-		presets1[0] = new TestPreset3();
+		presets1[0] = TEST_PRESET_3;
 		testApp.setPresets(presets1);
 		try {
 			assertEquals(0, new ServiceLevelCalculator(testApp).calculate());
@@ -59,17 +73,17 @@ public class ServiceLevelCalculatorTest extends TestCase {
 
 	public void testCalculate4() {
 		// Test 4: Preset 4 -> SL 3
-		presets1[0] = new TestPreset4();
+		presets1[0] = TEST_PRESET_4;
 		testApp.setPresets(presets1);
 		try {
 			assertEquals(3, new ServiceLevelCalculator(testApp).calculate());
 		} catch (RemoteException ignore) {
 		}
 	}
-	
-	public void testCalculate5() {	
+
+	public void testCalculate5() {
 		// Test 5: Preset 5 -> SL 0
-		presets1[0] = new TestPreset5();
+		presets1[0] = TEST_PRESET_5;
 		testApp.setPresets(presets1);
 		try {
 			assertEquals(0, new ServiceLevelCalculator(testApp).calculate());
@@ -79,54 +93,54 @@ public class ServiceLevelCalculatorTest extends TestCase {
 
 	public void testCalculate6() {
 		// Test 6: Preset 1 + Preset 2 -> SL 1
-		presets2[0] = new TestPreset1();
-		presets2[1] = new TestPreset2();
+		presets2[0] = TEST_PRESET_1;
+		presets2[1] = TEST_PRESET_2;
 		testApp.setPresets(presets2);
 		try {
 			assertEquals(1, new ServiceLevelCalculator(testApp).calculate());
 		} catch (RemoteException ignore) {
 		}
 	}
-	
+
 	public void testCalculate7() {
 		// Test 7: Preset 1 + Preset 3 -> SL 0
-		presets2[0] = new TestPreset1();
-		presets2[1] = new TestPreset3();
+		presets2[0] = TEST_PRESET_1;
+		presets2[1] = TEST_PRESET_3;
 		testApp.setPresets(presets2);
 		try {
 			assertEquals(0, new ServiceLevelCalculator(testApp).calculate());
 		} catch (RemoteException ignore) {
 		}
 	}
-	
+
 	public void testCalculate8() {
 
 		// Test 8: Preset 1 + Preset 4 -> SL 3
-		presets2[0] = new TestPreset1();
-		presets2[1] = new TestPreset4();
+		presets2[0] = TEST_PRESET_1;
+		presets2[1] = TEST_PRESET_4;
 		testApp.setPresets(presets2);
 		try {
 			assertEquals(3, new ServiceLevelCalculator(testApp).calculate());
 		} catch (RemoteException ignore) {
 		}
 	}
-	
+
 	public void testCalculate9() {
 		// Test 9: Preset 1 + Preset 5 -> SL 0
-		presets2[0] = new TestPreset1();
-		presets2[1] = new TestPreset5();
+		presets2[0] = TEST_PRESET_1;
+		presets2[1] = TEST_PRESET_5;
 		testApp.setPresets(presets2);
 		try {
 			assertEquals(0, new ServiceLevelCalculator(testApp).calculate());
 		} catch (RemoteException ignore) {
 		}
 	}
-	
+
 	public void testCalculate10() {
 
 		// Test 10: Preset 2 + Preset 3 -> SL 2
-		presets2[0] = new TestPreset2();
-		presets2[1] = new TestPreset3();
+		presets2[0] = TEST_PRESET_2;
+		presets2[1] = TEST_PRESET_3;
 		testApp.setPresets(presets2);
 		try {
 			assertEquals(2, new ServiceLevelCalculator(testApp).calculate());
@@ -136,8 +150,8 @@ public class ServiceLevelCalculatorTest extends TestCase {
 
 	public void testCalculate11() {
 		// Test 11: Preset 2 + Preset 4 -> SL 3
-		presets2[0] = new TestPreset2();
-		presets2[1] = new TestPreset4();
+		presets2[0] = TEST_PRESET_2;
+		presets2[1] = TEST_PRESET_4;
 		testApp.setPresets(presets2);
 		try {
 			assertEquals(3, new ServiceLevelCalculator(testApp).calculate());
@@ -147,8 +161,8 @@ public class ServiceLevelCalculatorTest extends TestCase {
 
 	public void testCalculate12() {
 		// Test 12: Preset 2 + Preset 5 -> SL 3
-		presets2[0] = new TestPreset2();
-		presets2[1] = new TestPreset5();
+		presets2[0] = TEST_PRESET_2;
+		presets2[1] = TEST_PRESET_5;
 		testApp.setPresets(presets2);
 		try {
 			assertEquals(3, new ServiceLevelCalculator(testApp).calculate());
@@ -158,8 +172,8 @@ public class ServiceLevelCalculatorTest extends TestCase {
 
 	public void testCalculate13() {
 		// Test 13: Preset 3 + Preset 4 -> SL 3
-		presets2[0] = new TestPreset3();
-		presets2[1] = new TestPreset4();
+		presets2[0] = TEST_PRESET_3;
+		presets2[1] = TEST_PRESET_4;
 		testApp.setPresets(presets2);
 		try {
 			assertEquals(3, new ServiceLevelCalculator(testApp).calculate());
@@ -169,8 +183,8 @@ public class ServiceLevelCalculatorTest extends TestCase {
 
 	public void testCalculate14() {
 		// Test 14: Preset 3 + Preset 5 -> SL 2
-		presets2[0] = new TestPreset3();
-		presets2[1] = new TestPreset5();
+		presets2[0] = TEST_PRESET_3;
+		presets2[1] = TEST_PRESET_5;
 		testApp.setPresets(presets2);
 		try {
 			assertEquals(2, new ServiceLevelCalculator(testApp).calculate());
@@ -178,10 +192,10 @@ public class ServiceLevelCalculatorTest extends TestCase {
 		}
 	}
 
-		// Test 15: Preset 4 + Preset 5 -> SL 4
+	// Test 15: Preset 4 + Preset 5 -> SL 4
 	public void testCalculate15() {
-		presets2[0] = new TestPreset4();
-		presets2[1] = new TestPreset5();
+		presets2[0] = TEST_PRESET_4;
+		presets2[1] = TEST_PRESET_5;
 		testApp.setPresets(presets2);
 		try {
 			assertEquals(4, new ServiceLevelCalculator(testApp).calculate());
@@ -191,9 +205,9 @@ public class ServiceLevelCalculatorTest extends TestCase {
 
 	public void testCalculate16() {
 		// Test 16: Preset 2 + Preset 3 + Preset 4 -> SL 3
-		presets3[0] = new TestPreset2();
-		presets3[1] = new TestPreset3();
-		presets3[2] = new TestPreset4();
+		presets3[0] = TEST_PRESET_2;
+		presets3[1] = TEST_PRESET_3;
+		presets3[2] = TEST_PRESET_4;
 		testApp.setPresets(presets3);
 		try {
 			assertEquals(3, new ServiceLevelCalculator(testApp).calculate());
@@ -203,9 +217,9 @@ public class ServiceLevelCalculatorTest extends TestCase {
 
 	public void testCalculate17() {
 		// Test 17: Preset 3 + Preset 4 + Preset 5 -> SL 4
-		presets3[0] = new TestPreset3();
-		presets3[1] = new TestPreset4();
-		presets3[2] = new TestPreset5();
+		presets3[0] = TEST_PRESET_3;
+		presets3[1] = TEST_PRESET_4;
+		presets3[2] = TEST_PRESET_5;
 		testApp.setPresets(presets3);
 		try {
 			assertEquals(4, new ServiceLevelCalculator(testApp).calculate());
@@ -215,9 +229,9 @@ public class ServiceLevelCalculatorTest extends TestCase {
 
 	public void testCalculate18() {
 		// Test 18: Preset 1 + Preset 2 + Preset 4 -> SL 3
-		presets3[0] = new TestPreset1();
-		presets3[1] = new TestPreset2();
-		presets3[2] = new TestPreset4();
+		presets3[0] = TEST_PRESET_1;
+		presets3[1] = TEST_PRESET_2;
+		presets3[2] = TEST_PRESET_4;
 		testApp.setPresets(presets3);
 		try {
 			assertEquals(3, new ServiceLevelCalculator(testApp).calculate());
