59d0cf88ec265620e3de5f762ef1b4dee08fbf79
Marcus Vetter <marqus.ve@googlemail.com>
2011-09-22 12:30:17 +0200

    added initial email app project refs #77

---
+++ pmp-android/Apps/EMailApp/src/de/unistuttgart/ipvs/pmp/apps/emailapp/EMailAppActivity.java
@@ -0,0 +1,13 @@
+package de.unistuttgart.ipvs.pmp.apps.emailapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EMailAppActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
