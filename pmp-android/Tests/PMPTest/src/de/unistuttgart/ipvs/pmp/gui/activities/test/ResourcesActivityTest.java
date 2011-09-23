7d3fcf0a0d5163495614c9f0fd64e0311e91641d
Andre <Andre@192.168.2.148>
2011-09-23 16:59:49 +0200

    testing long description scrollbar #refs 132

--- pmp-android/Tests/PMPTest/src/de/unistuttgart/ipvs/pmp/gui/activities/test/ResourcesActivityTest.java
+++ pmp-android/Tests/PMPTest/src/de/unistuttgart/ipvs/pmp/gui/activities/test/ResourcesActivityTest.java
@@ -53,7 +53,7 @@ public class ResourcesActivityTest extends ActivityInstrumentationTestCase2<Ress
         
         this.TEST_RES1_IDENT = "TEST_RES1";
         this.TEST_RES1_NAME = "TEST RESOURCE";
-        this.TEST_RES1_DESC = "TEST_DESCRIPTION";
+        this.TEST_RES1_DESC = "This is a very long test description to see if a scrollbar appears, if not this test is an absolute failure and i will kill myself today. if it does i am the happiest person alive, that ever existed in this beautiful world. i am so happy. so happy that i could jump up and down all day until my legs break into half and i am yet to be unhappy like the test failed. then again i dont care about the test, because i want to eat. i like to eat very much. in fact i like to eat so much that i am eating my own legs that were broken from the jumping up and down all day. man my legs are so delicious that i could be happy again, so happy like this stupid test is running perfectly normal. i dunno if this text is long enough but ill just keep writing till my fingers break into half to and then ill eat'em up until my stomachs full because im am very hungry. so hungry from all this stupid writing until the text is so long that this stupid scrollbar is appearing. man i am hungry!. this better be long enough...........";
         
         DatabaseOpenHelper doh = DatabaseSingleton.getInstance().getDatabaseHelper();
         doh.cleanTables();
