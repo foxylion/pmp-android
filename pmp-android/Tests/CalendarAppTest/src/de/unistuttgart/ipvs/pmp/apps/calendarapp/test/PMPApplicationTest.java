f2f7217138d3bc075af8ef7cd0fc4fe9f1c08aa9
Tobias Kuhn <kuhnts@studi.informatik.uni-stuttgart.de>
2011-09-25 18:07:32 +0200

    actual dummy test, refs #101

---
+++ pmp-android/Tests/CalendarAppTest/src/de/unistuttgart/ipvs/pmp/apps/calendarapp/test/PMPApplicationTest.java
@@ -0,0 +1,12 @@
+package de.unistuttgart.ipvs.pmp.apps.calendarapp.test;
+
+import android.test.ApplicationTestCase;
+import de.unistuttgart.ipvs.pmp.apps.calendarapp.CalendarApp;
+
+public class PMPApplicationTest extends ApplicationTestCase<CalendarApp> {
+    
+    public PMPApplicationTest() {
+        super(CalendarApp.class);
+    }
+    
+}
