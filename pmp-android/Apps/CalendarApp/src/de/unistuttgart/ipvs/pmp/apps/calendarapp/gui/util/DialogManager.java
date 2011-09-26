6237bf5531c84c8dcacfdb174504e60039d7ec0d
Thorsten Berberich <thorsch.b@googlemail.com>
2011-09-26 15:15:08 +0200

    Changed dialog refs #76

--- pmp-android/Apps/CalendarApp/src/de/unistuttgart/ipvs/pmp/apps/calendarapp/gui/util/DialogManager.java
+++ pmp-android/Apps/CalendarApp/src/de/unistuttgart/ipvs/pmp/apps/calendarapp/gui/util/DialogManager.java
@@ -1,9 +1,11 @@
 package de.unistuttgart.ipvs.pmp.apps.calendarapp.gui.util;
 
+import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.ComponentName;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Handler;
 import android.view.View;
@@ -11,6 +13,7 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import de.unistuttgart.ipvs.pmp.apps.calendarapp.CalendarApp;
 import de.unistuttgart.ipvs.pmp.apps.calendarapp.R;
+import de.unistuttgart.ipvs.pmp.apps.calendarapp.gui.activities.CalendarAppActivity;
 import de.unistuttgart.ipvs.pmp.apps.calendarapp.model.Model;
 
 public class DialogManager {
@@ -62,43 +65,31 @@ public class DialogManager {
      * Shows a dialog when the user wants to do sth. that is not allowed in this service level
      */
     public void showServiceLevelInsufficientDialog(Context context) {
-        final Dialog serviceLevelDialog = new Dialog(context);
-        serviceLevelDialog.setContentView(R.layout.serviceleveldialog);
         
-        serviceLevelDialog.setTitle(Model.getInstance().getContext().getString(R.string.insufficent_sl));
-        serviceLevelDialog.show();
-        
-        /*
-         * Closes the dialog
-         */
-        Button ok = (Button) serviceLevelDialog.findViewById(R.id.okButton);
-        ok.setOnClickListener(new OnClickListener() {
-            
-            @Override
-            public void onClick(View v) {
-                serviceLevelDialog.dismiss();
-            }
-        });
-        
-        /*
-         * Calls the PMP interface with an intent
-         */
-        Button changeSl = (Button) serviceLevelDialog.findViewById(R.id.changeSlButton);
-        changeSl.setOnClickListener(new OnClickListener() {
-            
-            @Override
-            public void onClick(View v) {
-                /*
-                 * Call Privacy Level Activity with the specified Intent
-                 */
-                Intent intent = new Intent();
-                intent.setComponent(new ComponentName("de.unistuttgart.ipvs.pmp",
-                        "de.unistuttgart.ipvs.pmp.gui.activities.ServiceLvlActivity"));
-                intent.putExtra("connection.identifier", "de.unistuttgart.ipvs.pmp.apps.calendarapp");
-                
-                Model.getInstance().getContext().startActivity(intent);
-                serviceLevelDialog.dismiss();
-            }
-        });
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle(context.getString(R.string.insufficent_sl))
+                .setMessage(context.getString(R.string.insufficent_sl_message))
+                .setPositiveButton(context.getString(R.string.ok), new DialogInterface.OnClickListener() {
+                    // Close the dialog
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.dismiss();
+                    }
+                }).setNegativeButton(context.getString(R.string.change_sl), new DialogInterface.OnClickListener() {
+                    
+                    public void onClick(DialogInterface dialog, int id) {
+                        /*
+                         * Call Privacy Level Activity with the specified Intent
+                         */
+                        Intent intent = new Intent();
+                        intent.setComponent(new ComponentName("de.unistuttgart.ipvs.pmp",
+                                "de.unistuttgart.ipvs.pmp.gui.activities.ServiceLvlActivity"));
+                        intent.putExtra("connection.identifier", "de.unistuttgart.ipvs.pmp.apps.calendarapp");
+                        
+                        Model.getInstance().getContext().startActivity(intent);
+                        dialog.dismiss();
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
     }
 }
