17121eba364ed8bec265f407535344c8d6b57d38
Alexander Wassiljew <alexanderwassiljew@googlemail.com>
2011-11-28 23:10:31 +0100

    vHike Project fixed, Activity and packages added refs 276

---
+++ pmp-android/Apps/vHike/src/de/unistuttgart/ipvs/pmp/apps/vhike/gui/VHikeActivity.java
@@ -0,0 +1,14 @@
+package de.unistuttgart.ipvs.pmp.apps.vhike.gui;
+
+import de.unistuttgart.ipvs.pmp.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class VHikeActivity extends Activity{
+
+		@Override
+		protected void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+			setContentView(R.layout.main);
+		}
+}
