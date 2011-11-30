85153502531159088781fdb9ce3ee92e4c344635
Alexander Wassiljew <alexanderwassiljew@googlemail.com>
2011-11-30 19:22:29 +0100

    JSonRequestReader and Provider implemented  refs 282

--- pmp-android/Apps/vHike/src/de/unistuttgart/ipvs/pmp/apps/vhike/Constants.java
+++ pmp-android/Apps/vHike/src/de/unistuttgart/ipvs/pmp/apps/vhike/Constants.java
@@ -10,7 +10,7 @@ public class Constants {
 	/*
 	 * URL of the webservice which is used by {@link JSonRequestReader} 
 	 */
-	public static final String WEBSERVICE_URL ="http://webservice.php";
+	public static final String WEBSERVICE_URL ="http://vhike.no-ip.org/test/posttest.php";
 	
 	
 }
