908c6616d4e95786087628bb8b2acfa506909f5f
Anton Makarov <anton.makarov.swt@googlemail.com>
2011-12-01 15:32:55 +0100

    HistoryActivity refs 282

--- pmp-android/Apps/vHike/src/de/unistuttgart/ipvs/pmp/apps/vhike/gui/HistoryActivity.java
+++ pmp-android/Apps/vHike/src/de/unistuttgart/ipvs/pmp/apps/vhike/gui/HistoryActivity.java
@@ -1,34 +1,50 @@
 package de.unistuttgart.ipvs.pmp.apps.vhike.gui;
 
-import de.unistuttgart.ipvs.pmp.R;
-import android.app.Activity;
-import android.content.Intent;
+import android.app.ListActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
+import android.widget.ArrayAdapter;
+
+//import de.unistuttgart.ipvs.pmp.R;
+//import android.app.Activity;
+//import android.content.Context;
+//import android.content.Intent;
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.View.OnClickListener;
+//import android.view.ViewGroup;
+//import android.widget.BaseAdapter;
+//import android.widget.Button;
+//import android.widget.LinearLayout;
+//import android.content.Context;
+//
+//import java.util.List;
+//
+//import android.content.Context;
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.widget.BaseAdapter;
+//import android.widget.ImageView;
+//import android.widget.LinearLayout;
+//import android.widget.TextView;
+//import de.unistuttgart.ipvs.pmp.gui.activity.AppsActivity;
+//import de.unistuttgart.ipvs.pmp.model.element.app.IApp;
 /**
- * This Activity gives user freedom to act as a Driver or Passenger
+ * This Activity gives user the List of all driven journeys
  * 
  * @author Anton Makarov
  * 
  */
-public class HistoryActivity extends Activity {
+public class HistoryActivity extends ListActivity {
 
+	
+	static final String[] DRIVES = new String[] {
+        "Berlin 12.03.2002", "Stuttgart 28.09.2010", "..."};
+	
+	@Override
 	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_ride);
-
-		registerListener();
-	}
-
-	private void registerListener() {
-//		Button btnDrive = (Button) findViewById(R.id.Button_Drive);
-//		Button btnSearch = (Button) findViewById(R.id.Button_Search);
-
-
-
-
+	        super.onCreate(savedInstanceState);
+	        setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, DRIVES));
 	}
 
 }
