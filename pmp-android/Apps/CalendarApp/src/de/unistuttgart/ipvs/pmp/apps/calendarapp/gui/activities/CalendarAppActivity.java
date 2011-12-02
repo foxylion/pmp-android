3db7f1c5439e7dcffa745529e2cf1ff959778182
Thorsten Berberich <thorsch.b@googlemail.com>
2011-12-02 21:53:36 +0100

    changed AIS.xml identifiers changed CalendarApp to work better with new API refs #229

--- pmp-android/Apps/CalendarApp/src/de/unistuttgart/ipvs/pmp/apps/calendarapp/gui/activities/CalendarAppActivity.java
+++ pmp-android/Apps/CalendarApp/src/de/unistuttgart/ipvs/pmp/apps/calendarapp/gui/activities/CalendarAppActivity.java
@@ -2,7 +2,7 @@
  * Copyright 2011 pmp-android development team
  * Project: CalendarApp
  * Project-Site: http://code.google.com/p/pmp-android/
- *
+ * 
  * ---------------------------------------------------------------------
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -30,6 +30,7 @@ import android.app.ListActivity;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.os.Looper;
 import android.os.RemoteException;
 import android.view.ContextMenu;
 import android.view.ContextMenu.ContextMenuInfo;
@@ -42,6 +43,7 @@ import android.widget.AdapterView.AdapterContextMenuInfo;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 import de.unistuttgart.ipvs.pmp.Log;
 import de.unistuttgart.ipvs.pmp.app.App;
 import de.unistuttgart.ipvs.pmp.apps.calendarapp.CalendarApp;
@@ -54,6 +56,8 @@ import de.unistuttgart.ipvs.pmp.apps.calendarapp.model.Appointment;
 import de.unistuttgart.ipvs.pmp.apps.calendarapp.model.Model;
 import de.unistuttgart.ipvs.pmp.apps.calendarapp.sqlConnector.SqlConnector;
 import de.unistuttgart.ipvs.pmp.resourcegroups.email.IEmailOperations;
+import de.unistuttgart.ipvs.pmp.service.utils.AbstractConnector;
+import de.unistuttgart.ipvs.pmp.service.utils.AbstractConnectorCallback;
 import de.unistuttgart.ipvs.pmp.service.utils.PMPServiceConnector;
 
 public class CalendarAppActivity extends ListActivity {
@@ -81,43 +85,37 @@ public class CalendarAppActivity extends ListActivity {
         
         this.appContext = getApplicationContext();
         
-//        // Connector to check if the app is registered yet
-//        final PMPServiceConnector connector = new PMPServiceConnector(this.appContext,
-//                ((CalendarApp) this.appContext).getSignee());
-//        connector.addCallbackHandler(new IConnectorCallback() {
-//            
-//            @Override
-//            public void disconnected() {
-//                Log.e("Disconnected");
-//            }
-//            
-//            
-//            @Override
-//            public void connected() {
-//                
-//                // Check if the service is registered yet
-//                if (!connector.isRegistered()) {
-//                    DialogManager.getInstance().showWaitingDialog();
-//                    
-//                    // Register the service
-//                    ((App) getApplication()).register(getApplicationContext());
-//                    
-//                    connector.unbind();
-//                } else {
-//                    Log.v("App already registered");
-//                    connector.unbind();
-//                }
-//            }
-//            
-//            
-//            @Override
-//            public void bindingFailed() {
-//                Log.e("Binding failed during registering app.");
-//            }
-//        });
-//        
-//        // Connect to the service
-//        connector.bind();
+        // Connector to check if the app is registered yet
+        final PMPServiceConnector pmpconnector = new PMPServiceConnector(this.appContext);
+        pmpconnector.addCallbackHandler(new AbstractConnectorCallback() {
+            
+            @Override
+            public void onConnect(AbstractConnector connector) throws RemoteException {
+                // Check if the service is registered yet
+                if (!pmpconnector.getAppService().isRegistered(getPackageName())) {
+                    Log.v("Registering");
+                    Looper.prepare();
+                    DialogManager.getInstance().showWaitingDialog();
+                    Looper.loop();
+                    pmpconnector.getAppService().registerApp(getPackageName());
+                    pmpconnector.unbind();
+                } else {
+                    Log.v("App registered");
+                }
+            }
+            
+            
+            @Override
+            public void onBindingFailed(AbstractConnector connector) {
+                Looper.prepare();
+                Toast toast = Toast.makeText(self, "Could not register app", Toast.LENGTH_LONG);
+                toast.show();
+                Looper.loop();
+                
+            }
+        });
+        // Connect to the service
+        pmpconnector.bind();
         
         setContentView(R.layout.list_layout);
         
@@ -165,75 +163,73 @@ public class CalendarAppActivity extends ListActivity {
     
     @Override
     public boolean onContextItemSelected(MenuItem aItem) {
-//        // Get the service level
-//        int serviceLevel = Model.getInstance().getServiceLevel();
-//        
-//        // The menu information
-//        AdapterContextMenuInfo menuInfo = (AdapterContextMenuInfo) aItem.getMenuInfo();
-//        final Appointment clicked = Model.getInstance().getAppointmentByIndex(menuInfo.position);
-//        /*
-//         * Called when the user presses sth. in the menu that appears while long clicking
-//         */
-//        if (aItem.getItemId() == 0) {
-//            if (serviceLevel >= 2) {
-//                SqlConnector.getInstance().deleteAppointment(clicked.getId());
-//            } else {
-//                DialogManager.getInstance().showServiceLevelInsufficientDialog(this);
-//            }
-//            
-//            return true;
-//        }
-//        if (aItem.getItemId() == 1) {
-//            if (serviceLevel == 3 || serviceLevel == 5 || serviceLevel == 7) {
-//                /*
-//                 * Connect to the EmailResourceGroup and send an mail with the date
-//                 */
-//                final String resGroupId = "de.unistuttgart.ipvs.pmp.resourcegroups.email";
-//                final ResourceGroupServiceConnector resGroupCon = new ResourceGroupServiceConnector(this.appContext,
-//                        ((CalendarApp) this.appContext).getSignee(), resGroupId);
-//                resGroupCon.addCallbackHandler(new IConnectorCallback() {
-//                    
-//                    @Override
-//                    public void disconnected() {
-//                        Log.d("Disconnected from " + resGroupId);
-//                    }
-//                    
-//                    
-//                    @Override
-//                    public void connected() {
-//                        Log.d("Connected to " + resGroupId);
-//                        try {
-//                            IEmailOperations emailOP = IEmailOperations.Stub.asInterface(resGroupCon.getAppService()
-//                                    .getResource("emailOperations"));
-//                            if (emailOP != null) {
-//                                Calendar cal = new GregorianCalendar();
-//                                cal.setTime(clicked.getDate());
-//                                SimpleDateFormat formatter;
-//                                formatter = new SimpleDateFormat("dd.MM.yyyy", Locale.getDefault());
-//                                emailOP.sendEmail("", getString(R.string.subject), getString(R.string.appoint)
-//                                        + formatter.format(cal.getTime()) + "\n" + getString(R.string.desc) + ": "
-//                                        + clicked.getDescrpition());
-//                            }
-//                        } catch (RemoteException e) {
-//                            Log.e("Remote Exception: ", e);
-//                        } finally {
-//                            resGroupCon.unbind();
-//                        }
-//                    }
-//                    
-//                    
-//                    @Override
-//                    public void bindingFailed() {
-//                        Log.e("Binding failed to " + resGroupId);
-//                    }
-//                });
-//                resGroupCon.bind();
-//                return true;
-//            } else {
-//                DialogManager.getInstance().showServiceLevelInsufficientDialog(this);
-//            }
-//            
-//        }
+        // The menu information
+        AdapterContextMenuInfo menuInfo = (AdapterContextMenuInfo) aItem.getMenuInfo();
+        final Appointment clicked = Model.getInstance().getAppointmentByIndex(menuInfo.position);
+        /*
+         * Called when the user presses sth. in the menu that appears while long clicking
+         */
+        if (aItem.getItemId() == 0) {
+            if (((App) getApplication()).isServiceFeatureEnabled("write")) {
+                SqlConnector.getInstance().deleteAppointment(clicked.getId());
+            } else {
+                DialogManager.getInstance().showServiceLevelInsufficientDialog(this);
+            }
+            
+            return true;
+        }
+        if (aItem.getItemId() == 1) {
+            if (((App) getApplication()).isServiceFeatureEnabled("send")
+                    && ((App) getApplication()).isServiceFeatureEnabled("read")) {
+                /*
+                 * Connect to the EmailResourceGroup and send an mail with the date
+                 */
+                final String resGroupId = "de.unistuttgart.ipvs.pmp.resourcegroups.email";
+                //                        final ResourceGroupServiceConnector resGroupCon = new ResourceGroupServiceConnector(this.appContext,
+                //                                ((CalendarApp) this.appContext).getSignee(), resGroupId);
+                //                        resGroupCon.addCallbackHandler(new IConnectorCallback() {
+                //                            
+                //                            @Override
+                //                            public void disconnected() {
+                //                                Log.d("Disconnected from " + resGroupId);
+                //                            }
+                //                            
+                //                            
+                //                            @Override
+                //                            public void connected() {
+                //                                Log.d("Connected to " + resGroupId);
+                //                                try {
+                //                                    IEmailOperations emailOP = IEmailOperations.Stub.asInterface(resGroupCon.getAppService()
+                //                                            .getResource("emailOperations"));
+                //                                    if (emailOP != null) {
+                //                                        Calendar cal = new GregorianCalendar();
+                //                                        cal.setTime(clicked.getDate());
+                //                                        SimpleDateFormat formatter;
+                //                                        formatter = new SimpleDateFormat("dd.MM.yyyy", Locale.getDefault());
+                //                                        emailOP.sendEmail("", getString(R.string.subject), getString(R.string.appoint)
+                //                                                + formatter.format(cal.getTime()) + "\n" + getString(R.string.desc) + ": "
+                //                                                + clicked.getDescrpition());
+                //                                    }
+                //                                } catch (RemoteException e) {
+                //                                    Log.e("Remote Exception: ", e);
+                //                                } finally {
+                //                                    resGroupCon.unbind();
+                //                                }
+                //                            }
+                //                            
+                //                            
+                //                            @Override
+                //                            public void bindingFailed() {
+                //                                Log.e("Binding failed to " + resGroupId);
+                //                            }
+                //                        });
+                //                        resGroupCon.bind();
+                return true;
+            } else {
+                DialogManager.getInstance().showServiceLevelInsufficientDialog(this);
+            }
+            
+        }
         return false;
     }
     
@@ -260,66 +256,66 @@ public class CalendarAppActivity extends ListActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         return false;
         // Get the service level
-//        int serviceLevel = Model.getInstance().getServiceLevel();
-//        
-//        // Handle item selection
-//        switch (item.getItemId()) {
-//            case R.id.new_appointment:
-//                if (serviceLevel >= 2) {
-//                    // Show the new appointment dialog
-//                    Dialog dialog = new NewAppointmentDialog(Model.getInstance().getContext());
-//                    dialog.setTitle("Create new appointment");
-//                    dialog.show();
-//                } else {
-//                    DialogManager.getInstance().showServiceLevelInsufficientDialog(this);
-//                }
-//                return true;
-//            case R.id.delete_all_appointments:
-//                if (serviceLevel >= 2) {
-//                    // Show the confirm dialog for deleting all appointments
-//                    new AlertDialog.Builder(this).setIcon(android.R.drawable.ic_dialog_alert)
-//                            .setTitle(R.string.delete_all_appointments)
-//                            .setMessage(R.string.delete_all_appointments_question)
-//                            .setPositiveButton(R.string.conf, new DialogInterface.OnClickListener() {
-//                                
-//                                @Override
-//                                public void onClick(DialogInterface dialog, int which) {
-//                                    Model.getInstance().deleteAllAppointments();
-//                                }
-//                                
-//                            }).show();
-//                } else {
-//                    DialogManager.getInstance().showServiceLevelInsufficientDialog(this);
-//                }
-//                return true;
-//            case R.id.import_appointments:
-//                if (serviceLevel >= 4) {
-//                    /*
-//                     * Fill the list of files for importing.
-//                     * It is also used to check for exporting, if a file already exists.
-//                     */
-//                    FileSystemConnector.getInstance().listStoredFiles(FileSystemListActionType.IMPORT);
-//                    
-//                } else {
-//                    DialogManager.getInstance().showServiceLevelInsufficientDialog(this);
-//                }
-//                return true;
-//            case R.id.export_appointments:
-//                if (serviceLevel >= 6) {
-//                    /*
-//                     * Fill the list of files for importing.
-//                     * It is also used to check for exporting, if a file already exists.
-//                     */
-//                    FileSystemConnector.getInstance().listStoredFiles(FileSystemListActionType.EXPORT);
-//                    
-//                } else {
-//                    DialogManager.getInstance().showServiceLevelInsufficientDialog(this);
-//                }
-//                
-//                return true;
-//            default:
-//                return super.onOptionsItemSelected(item);
-//        }
+        //        int serviceLevel = Model.getInstance().getServiceLevel();
+        //        
+        //        // Handle item selection
+        //        switch (item.getItemId()) {
+        //            case R.id.new_appointment:
+        //                if (serviceLevel >= 2) {
+        //                    // Show the new appointment dialog
+        //                    Dialog dialog = new NewAppointmentDialog(Model.getInstance().getContext());
+        //                    dialog.setTitle("Create new appointment");
+        //                    dialog.show();
+        //                } else {
+        //                    DialogManager.getInstance().showServiceLevelInsufficientDialog(this);
+        //                }
+        //                return true;
+        //            case R.id.delete_all_appointments:
+        //                if (serviceLevel >= 2) {
+        //                    // Show the confirm dialog for deleting all appointments
+        //                    new AlertDialog.Builder(this).setIcon(android.R.drawable.ic_dialog_alert)
+        //                            .setTitle(R.string.delete_all_appointments)
+        //                            .setMessage(R.string.delete_all_appointments_question)
+        //                            .setPositiveButton(R.string.conf, new DialogInterface.OnClickListener() {
+        //                                
+        //                                @Override
+        //                                public void onClick(DialogInterface dialog, int which) {
+        //                                    Model.getInstance().deleteAllAppointments();
+        //                                }
+        //                                
+        //                            }).show();
+        //                } else {
+        //                    DialogManager.getInstance().showServiceLevelInsufficientDialog(this);
+        //                }
+        //                return true;
+        //            case R.id.import_appointments:
+        //                if (serviceLevel >= 4) {
+        //                    /*
+        //                     * Fill the list of files for importing.
+        //                     * It is also used to check for exporting, if a file already exists.
+        //                     */
+        //                    FileSystemConnector.getInstance().listStoredFiles(FileSystemListActionType.IMPORT);
+        //                    
+        //                } else {
+        //                    DialogManager.getInstance().showServiceLevelInsufficientDialog(this);
+        //                }
+        //                return true;
+        //            case R.id.export_appointments:
+        //                if (serviceLevel >= 6) {
+        //                    /*
+        //                     * Fill the list of files for importing.
+        //                     * It is also used to check for exporting, if a file already exists.
+        //                     */
+        //                    FileSystemConnector.getInstance().listStoredFiles(FileSystemListActionType.EXPORT);
+        //                    
+        //                } else {
+        //                    DialogManager.getInstance().showServiceLevelInsufficientDialog(this);
+        //                }
+        //                
+        //                return true;
+        //            default:
+        //                return super.onOptionsItemSelected(item);
+        //        }
     }
     
     
