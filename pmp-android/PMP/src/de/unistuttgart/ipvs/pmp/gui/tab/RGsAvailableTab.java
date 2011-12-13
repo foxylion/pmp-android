60d29f17fd8bd3962e0aabc1c1e160827b63f654
Frieder Schüler <frieder.schueler@gmail.com>
2011-12-13 07:13:21 +0100

    Added more functionality for RGsActivity

--- pmp-android/PMP/src/de/unistuttgart/ipvs/pmp/gui/tab/RGsAvailableTab.java
+++ pmp-android/PMP/src/de/unistuttgart/ipvs/pmp/gui/tab/RGsAvailableTab.java
@@ -3,29 +3,41 @@ package de.unistuttgart.ipvs.pmp.gui.tab;
 import java.util.Arrays;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
+import de.unistuttgart.ipvs.pmp.gui.activity.RGDetailsActivity;
 import de.unistuttgart.ipvs.pmp.gui.adapter.RGsAdapter;
 import de.unistuttgart.ipvs.pmp.gui.placeholder.ModelProxy;
 import de.unistuttgart.ipvs.pmp.model.element.resourcegroup.IResourceGroup;
 
 public class RGsAvailableTab extends ListActivity {
     
+    protected IResourceGroup[] resourceGroups;
+    
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
-        IResourceGroup[] resourceGroups = ModelProxy.get().getResourceGroups();
+        resourceGroups = ModelProxy.get().getResourceGroups();
         RGsAdapter rgsAdapter = new RGsAdapter(this, Arrays.asList(resourceGroups));
         this.setListAdapter(rgsAdapter);
         
-        /*
-        appsList.setOnItemClickListener(new OnItemClickListener() {
+        ListView lv = getListView();
+        lv.setTextFilterEnabled(true);
+        
+        lv.setOnItemClickListener(new OnItemClickListener() {
             
-            @Override
-            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
-                Toast.makeText(AppsActivity.this, "Tapped on item " + arg2, Toast.LENGTH_SHORT).show();
-                Intent i = new Intent(AppsActivity.this, AppActivity.class);
-                AppsActivity.this.startActivity(i);
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent i = new Intent(RGsAvailableTab.this, RGDetailsActivity.class);
+                String identifier = resourceGroups[position].getIdentifier();
+                i.putExtra("identifier", identifier);
+                RGsAvailableTab.this.startActivity(i);
             }
-            */
+            
+        });
+        
     }
 }
