c44592bbde8648bc8b5baea1002c6e6b6eb088a4
Frieder Schüler <frieder.schueler@gmail.com>
2011-12-16 14:18:30 +0100

    added installation task

--- pmp-android/PMP/src/de/unistuttgart/ipvs/pmp/pluginmanager/tasks/InstallPluginTask.java
+++ pmp-android/PMP/src/de/unistuttgart/ipvs/pmp/pluginmanager/tasks/InstallPluginTask.java
@@ -1,25 +1,33 @@
 package de.unistuttgart.ipvs.pmp.pluginmanager.tasks;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.Locale;
-import com.google.gson.Gson;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipException;
+import java.util.zip.ZipFile;
+import java.util.zip.ZipInputStream;
+
 import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import de.unistuttgart.ipvs.pmp.Log;
+import de.unistuttgart.ipvs.pmp.PMPApplication;
 import de.unistuttgart.ipvs.pmp.R;
 import de.unistuttgart.ipvs.pmp.gui.activity.RGDetailsActivity;
-import de.unistuttgart.ipvs.pmp.gui.adapter.RGsAvailableAdapter;
 import de.unistuttgart.ipvs.pmp.gui.model.ModelProxy;
-import de.unistuttgart.ipvs.pmp.gui.tab.RGsAvailableTab;
-import de.unistuttgart.ipvs.pmp.model.element.resourcegroup.IResourceGroup;
-import de.unistuttgart.ipvs.pmp.pluginmanager.AvailablePlugins;
-import de.unistuttgart.ipvs.pmp.pluginmanager.PluginManager;
 
 public class InstallPluginTask extends AsyncTask<String, Integer, Void> {
     
     private RGDetailsActivity context;
+    
     
     public InstallPluginTask(RGDetailsActivity context) {
         this.context = context;
@@ -39,11 +47,10 @@ public class InstallPluginTask extends AsyncTask<String, Integer, Void> {
     
     @Override
     protected Void doInBackground(String... identifier) {
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+        if (fetchPlugin(identifier[0])) {
+            if (deployPlugin(identifier[0])) {
+                ModelProxy.get().installResourceGroup(identifier[0]);
+            }
         }
         return null;
     }
@@ -66,31 +73,73 @@ public class InstallPluginTask extends AsyncTask<String, Integer, Void> {
     }
     
     
-    private AvailablePlugins fetchAvailablePlugins() {
-        String json = "";
+    private boolean deployPlugin(String identifier) {
         try {
-            String lang = Locale.getDefault().getLanguage();
-            String urlString = "http://www.friederschueler.de/pmp/?lang=" + lang;
-            Log.e("Fetching new available Plugins from " + urlString);
+            File path = PMPApplication.getContext().getDir("plugins", 0);
+            File file = new File(path, identifier + ".jar");
+            InputStream input = new FileInputStream(file);
+            ZipInputStream zinput = new ZipInputStream(input);
+            ZipEntry entry;
+            while ((entry = zinput.getNextEntry()) != null) {
+                saveEntry(zinput, entry);
+            }
+            return true;
+        } catch (Exception e) {
+            Log.e(e.toString());
+            return false;
+        }
+    }
+    
+    
+    private void saveEntry(ZipInputStream zinput, ZipEntry target) throws ZipException, IOException {
+        int count;
+        byte data[] = new byte[8192];
+        Log.e(target.getName());
+        FileOutputStream fos = new FileOutputStream(target.getName());
+        BufferedOutputStream output = new BufferedOutputStream(fos);
+        while ((count = zinput.read(data, 0, 8192)) != -1) {
+            output.write(data, 0, count);
+        }
+        output.flush();
+        output.close();
+    }
+    
+    
+    private boolean fetchPlugin(String identifier) {
+        try {
+            String urlString = "http://www.friederschueler.de/pmp/" + identifier + ".jar";
+            
+            Log.e("Fetching Plugin " + identifier + " from " + urlString);
             URL url = new URL(urlString);
             URLConnection connection = url.openConnection();
             connection.connect();
-            long lenghtOfFile = connection.getContentLength();
-            BufferedInputStream input = new BufferedInputStream(url.openStream());
-            byte data[] = new byte[1024];
+            
+            File outputPath = PMPApplication.getContext().getDir("plugins", 0);
+            File outputFile = new File(outputPath, identifier + ".jar");
+            Log.e("Write Plugin to " + outputFile.toString());
+            
+            long lenght = connection.getContentLength();
+            
+            InputStream input = new BufferedInputStream(url.openStream());
+            FileOutputStream output = new FileOutputStream(outputFile);
+            
+            byte data[] = new byte[8192];
             long totalBytesRead = 0;
             int bytesRead;
             
             while ((bytesRead = input.read(data)) != -1) {
                 totalBytesRead += bytesRead;
-                publishProgress((int) (totalBytesRead * 100 / lenghtOfFile));
-                json += new String(data, 0, bytesRead);
+                publishProgress((int) (totalBytesRead * 100 / lenght));
+                output.write(data, 0, bytesRead);
             }
             
+            output.flush();
+            output.close();
             input.close();
+            return true;
         } catch (Exception e) {
             Log.e(e.toString());
+            return false;
         }
-        return new Gson().fromJson(json, AvailablePlugins.class);
     }
 }
