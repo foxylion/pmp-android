a1e71917491d062f584e0de4dd7313fa432f3a54
Marcus Vetter <marqus.ve@googlemail.com>
2011-12-17 18:00:32 +0100

    added the Privacy Settings Tab on Presets and build an expandable ListView refs #280

--- pmp-android/PMP/src/de/unistuttgart/ipvs/pmp/gui/adapter/PresetPrivacySettingsAdapter.java
+++ pmp-android/PMP/src/de/unistuttgart/ipvs/pmp/gui/adapter/PresetPrivacySettingsAdapter.java
@@ -1,66 +1,62 @@
 package de.unistuttgart.ipvs.pmp.gui.adapter;
 
-import java.util.List;
-
+import java.util.ArrayList;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
+import android.widget.BaseExpandableListAdapter;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 import de.unistuttgart.ipvs.pmp.R;
 import de.unistuttgart.ipvs.pmp.model.element.privacysetting.IPrivacySetting;
+import de.unistuttgart.ipvs.pmp.model.element.resourcegroup.IResourceGroup;
 
 /**
  * The {@link PresetPrivacySettingsAdapter} is the list of Privacy Settings in the {@link PresetPSsTabTab}.
  * 
  * @author Marcus Vetter
  */
-public class PresetPrivacySettingsAdapter extends BaseAdapter {
+public class PresetPrivacySettingsAdapter extends BaseExpandableListAdapter {
     
     private Context context;
-    private List<IPrivacySetting> privacySettings;
+    
+    private ArrayList<IResourceGroup> rgList;
+    
+    private ArrayList<ArrayList<IPrivacySetting>> psList;
     
     
-    public PresetPrivacySettingsAdapter(Context context, List<IPrivacySetting> privacySettings) {
+    public PresetPrivacySettingsAdapter(Context context, ArrayList<IResourceGroup> rgList,
+            ArrayList<ArrayList<IPrivacySetting>> psList) {
         this.context = context;
-        this.privacySettings = privacySettings;
+        this.rgList = rgList;
+        this.psList = psList;
     }
     
     
     @Override
-    public int getCount() {
-        return this.privacySettings.size();
+    public Object getChild(int groupPosition, int childPosition) {
+        return psList.get(groupPosition).get(childPosition);
     }
     
     
     @Override
-    public Object getItem(int position) {
-        return this.privacySettings.get(position);
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
     }
     
     
     @Override
-    public long getItemId(int position) {
-        return position;
-    }
-    
-    
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        IPrivacySetting ps = this.privacySettings.get(position);
+    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View entryView, ViewGroup parent) {
+        // Get the Privacy Setting
+        IPrivacySetting ps = (IPrivacySetting) getChild(groupPosition, childPosition);
         
-        /* load the layout from the xml file */
-        LayoutInflater inflater = (LayoutInflater) this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        LinearLayout entryView = (LinearLayout) inflater.inflate(R.layout.listitem_preset_ps, null);
+        // Inflate the layout
+        LayoutInflater infalInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        entryView = infalInflater.inflate(R.layout.listitem_preset_ps, null);
         
-        /* Set icon, name, value of the Privacy Setting */
-        ImageView icon = (ImageView) entryView.findViewById(R.id.ImageView_Icon);
-        icon.setImageDrawable(ps.getResourceGroup().getIcon());
-        
-        TextView name = (TextView) entryView.findViewById(R.id.TextView_Name);
+        // Set name and value of one Privacy Setting
+        TextView name = (TextView) entryView.findViewById(R.id.TextView_Name_PS);
         name.setText(ps.getName());
         
         TextView value = (TextView) entryView.findViewById(R.id.TextView_Value);
@@ -69,4 +65,66 @@ public class PresetPrivacySettingsAdapter extends BaseAdapter {
         
         return entryView;
     }
+    
+    
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        return psList.get(groupPosition).size();
+    }
+    
+    
+    @Override
+    public Object getGroup(int groupPosition) {
+        return rgList.get(groupPosition);
+    }
+    
+    
+    @Override
+    public int getGroupCount() {
+        return rgList.size();
+    }
+    
+    
+    @Override
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+    }
+    
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded, View entryView, ViewGroup parent) {
+        // Get the ResourceGroup
+        IResourceGroup rg = (IResourceGroup) getGroup(groupPosition);
+        
+        // Inflate the layout
+        LayoutInflater infalInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        entryView = infalInflater.inflate(R.layout.listitem_preset_rg, null);
+        
+        // Set name and icon of the ResourceGrouop
+        TextView name = (TextView) entryView.findViewById(R.id.TextView_RG_Name);
+        name.setText(rg.getName());
+        
+        ImageView icon = (ImageView) entryView.findViewById(R.id.ImageView_RG_Icon);
+        icon.setImageDrawable(rg.getIcon());
+        
+        return entryView;
+    }
+    
+    
+    @Override
+    public boolean hasStableIds() {
+        return true;
+    }
+    
+    
+    @Override
+    public boolean isChildSelectable(int arg0, int arg1) {
+        return true;
+    }
+    
+    
+    @Override
+    public boolean areAllItemsEnabled() {
+        return true;
+    }
+    
 }
--- pmp-android/PMP/src/de/unistuttgart/ipvs/pmp/gui/tab/PresetPrivacySettingsTab.java
+++ pmp-android/PMP/src/de/unistuttgart/ipvs/pmp/gui/tab/PresetPrivacySettingsTab.java
@@ -1,13 +1,20 @@
 package de.unistuttgart.ipvs.pmp.gui.tab;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuInflater;
-import android.widget.ListView;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.view.View.OnCreateContextMenuListener;
+import android.widget.ExpandableListView;
+import android.widget.ExpandableListView.OnChildClickListener;
 import android.widget.TextView;
 import de.unistuttgart.ipvs.pmp.R;
 import de.unistuttgart.ipvs.pmp.gui.adapter.PresetPrivacySettingsAdapter;
@@ -15,6 +22,7 @@ import de.unistuttgart.ipvs.pmp.gui.model.ModelProxy;
 import de.unistuttgart.ipvs.pmp.gui.util.GUIConstants;
 import de.unistuttgart.ipvs.pmp.model.element.preset.IPreset;
 import de.unistuttgart.ipvs.pmp.model.element.privacysetting.IPrivacySetting;
+import de.unistuttgart.ipvs.pmp.model.element.resourcegroup.IResourceGroup;
 
 /**
  * The "Assigned Privacy Settings" tab of a Preset
@@ -32,12 +40,17 @@ public class PresetPrivacySettingsTab extends Activity {
     /**
      * The Priavcy Setting list
      */
-    private List<IPrivacySetting> psList;
+    private List<IPrivacySetting> allPSList;
+    
+    /**
+     * The list with all RGs and their PSs
+     */
+    private ArrayList<ArrayList<IPrivacySetting>> psList;
     
     /**
      * The Privacy Setting list view
      */
-    private ListView psListView;
+    private ExpandableListView psExpandableListView;
     
     
     @Override
@@ -51,17 +64,21 @@ public class PresetPrivacySettingsTab extends Activity {
         // Set view
         setContentView(R.layout.tab_preset_pss);
         
+        // Initialize the data structures and add listener
         init();
         
+        // Update the list of RGs and PSs
         updateList();
         
     }
-
+    
+    
     @Override
     protected void onResume() {
         super.onResume();
         updateList();
     }
+    
     
     /**
      * Create the menu
@@ -73,12 +90,81 @@ public class PresetPrivacySettingsTab extends Activity {
         return super.onCreateOptionsMenu(menu);
     }
     
+    
     /**
      * Initialize the data structures
      */
     private void init() {
         // Setup the appsListView
-        this.psListView = (ListView) findViewById(R.id.listview_assigned_pss);
+        this.psExpandableListView = (ExpandableListView) findViewById(R.id.expandable_list_view_assigned_pss);
+        
+        this.psExpandableListView.setClickable(true);
+        this.psExpandableListView.setLongClickable(false);
+        registerForContextMenu(this.psExpandableListView);
+        
+        // Add a context menu listener for long clicks
+        this.psExpandableListView.setOnCreateContextMenuListener(new OnCreateContextMenuListener() {
+            
+            @Override
+            public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
+                
+                ExpandableListView.ExpandableListContextMenuInfo info = (ExpandableListView.ExpandableListContextMenuInfo) menuInfo;
+                int type = ExpandableListView.getPackedPositionType(info.packedPosition);
+                int rgPos = ExpandableListView.getPackedPositionGroup(info.packedPosition);
+                int psPos = ExpandableListView.getPackedPositionChild(info.packedPosition);
+                
+                // Open a context menu, if this was a click on a Privacy Setting (type=1)
+                if (type == 1) {
+                    
+                    IPrivacySetting ps = PresetPrivacySettingsTab.this.psList.get(rgPos).get(psPos);
+                    
+                    menu.setHeaderTitle(ps.getName());
+                    menu.setHeaderIcon(ps.getResourceGroup().getIcon());
+                    menu.add(0, 0, 0, R.string.preset_tab_pss_context_menu_change_value);
+                    menu.add(1, 1, 0, R.string.preset_tab_pss_context_menu_remove_pss);
+                }
+            }
+        });
+        
+        // React on clicked Privacy Setting (child)
+        this.psExpandableListView.setOnChildClickListener(new OnChildClickListener() {
+            
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id) {
+                openContextMenu(v);
+                return true;
+            }
+        });
+        
+    }
+    
+    
+    /**
+     * React on a selected context menu item
+     */
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        
+        // Parse the menu info
+        ExpandableListView.ExpandableListContextMenuInfo info = (ExpandableListView.ExpandableListContextMenuInfo) item
+                .getMenuInfo();
+        int rgPos = ExpandableListView.getPackedPositionGroup(info.packedPosition);
+        int psPos = ExpandableListView.getPackedPositionChild(info.packedPosition);
+        
+        // Get the Privacy Setting
+        IPrivacySetting ps = PresetPrivacySettingsTab.this.psList.get(rgPos).get(psPos);
+        
+        // Handle
+        switch (item.getItemId()) {
+            case 0: // Change PS value
+                return true;
+            case 1: // Remove PS
+                preset.removePrivacySetting(ps);
+                updateList();
+                return true;
+        }
+        
+        return false;
     }
     
     
@@ -87,24 +173,49 @@ public class PresetPrivacySettingsTab extends Activity {
      * 
      */
     public void updateList() {
+        allPSList = new ArrayList<IPrivacySetting>();
         
-        psList = new ArrayList<IPrivacySetting>();
+        /* Build a hash map with the RGs and their PSs */
+        HashMap<IResourceGroup, ArrayList<IPrivacySetting>> RGPSMap = new HashMap<IResourceGroup, ArrayList<IPrivacySetting>>();
         
         for (IPrivacySetting ps : preset.getGrantedPrivacySettings()) {
-            psList.add(ps);
+            IResourceGroup rg = ps.getResourceGroup();
+            
+            // Add the PS to the allPsList
+            allPSList.add(ps);
+            
+            if (!RGPSMap.containsKey(ps.getResourceGroup())) {
+                // The map does not contain the RG: Add it as key and the PS as value
+                ArrayList<IPrivacySetting> newList = new ArrayList<IPrivacySetting>();
+                newList.add(ps);
+                RGPSMap.put(rg, newList);
+            } else {
+                // The map already contains the RG: Add the PS
+                ArrayList<IPrivacySetting> existingList = RGPSMap.get(rg);
+                existingList.add(ps);
+            }
+            
         }
-         
         
-        PresetPrivacySettingsAdapter ppsAdapter = new PresetPrivacySettingsAdapter(this, psList);
-        psListView.setAdapter(ppsAdapter);
+        /* Build two lists, separated into RGs and PSs out of the map */
+        ArrayList<IResourceGroup> rgList = new ArrayList<IResourceGroup>();
+        psList = new ArrayList<ArrayList<IPrivacySetting>>();
+        
+        for (IResourceGroup rg : RGPSMap.keySet()) {
+            rgList.add(rg);
+            psList.add(RGPSMap.get(rg));
+        }
+        
+        // Add the adapter
+        PresetPrivacySettingsAdapter ppsAdapter = new PresetPrivacySettingsAdapter(this, rgList, psList);
+        psExpandableListView.setAdapter(ppsAdapter);
         
         // Show or hide the text view about no pss assigned
         TextView noAssignedPSs = (TextView) findViewById(R.id.preset_tab_pss_no_assigned);
-        if (psList.size() == 0) {
+        if (allPSList.size() == 0) {
             noAssignedPSs.setVisibility(TextView.VISIBLE);
         } else {
             noAssignedPSs.setVisibility(TextView.GONE);
         }
     }
-    
 }
