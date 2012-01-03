4e700bc3551e1289d8b34d45c8725741bbb2c324
Alexander Wassiljew <neon@ubuntu.(none)>
2012-01-04 00:35:57 +0100

    History refs #282

---
+++ pmp-android/Apps/vHike/src/de/unistuttgart/ipvs/pmp/apps/vhike/gui/HistoryMenuActivity.java
@@ -0,0 +1,54 @@
+package de.unistuttgart.ipvs.pmp.apps.vhike.gui;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+import de.unistuttgart.ipvs.pmp.R;
+
+public class HistoryMenuActivity extends Activity{
+	Button btn_driver;
+	Button btn_passenger;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		this.setContentView(R.layout.activity_history_menu);
+		
+		btn_driver = (Button) findViewById(R.id.Button_Driver_History);
+		btn_passenger = (Button) findViewById(R.id.Button_Passenger_History);
+		
+		createTouchListener(this.getBaseContext());
+	}
+	
+	private void createTouchListener(final Context context) {
+		btn_driver.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				Toast.makeText(context, "Driver", Toast.LENGTH_SHORT).show();
+				
+				Intent intent = new Intent(HistoryMenuActivity.this,
+						HistoryActivity.class);
+				intent.putExtra("IS_DRIVER", true);
+				HistoryMenuActivity.this.startActivity(intent);
+			}
+		});
+		btn_passenger.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				Toast.makeText(context, "Passenger", Toast.LENGTH_SHORT).show();
+				
+				Intent intent = new Intent(HistoryMenuActivity.this,
+						HistoryActivity.class);
+				intent.putExtra("IS_DRIVER", false);
+				HistoryMenuActivity.this.startActivity(intent);
+			}
+		});
+	}
+	
+	
+}
