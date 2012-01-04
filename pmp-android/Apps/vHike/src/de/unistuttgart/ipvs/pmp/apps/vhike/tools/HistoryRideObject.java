00132176fe30faf3ab691ad2b0946d1a8f64fa80
Alexander Wassiljew <neon@ubuntu.(none)>
2012-01-04 01:15:35 +0100

    History refs #282

---
+++ pmp-android/Apps/vHike/src/de/unistuttgart/ipvs/pmp/apps/vhike/tools/HistoryRideObject.java
@@ -0,0 +1,23 @@
+package de.unistuttgart.ipvs.pmp.apps.vhike.tools;
+
+public class HistoryRideObject {
+
+	String creation ="";
+	String destination="";
+	
+	
+	public HistoryRideObject(String creation, String destination) {
+		this.creation = creation;
+		this.destination = destination;
+	}
+	
+
+	public String getCreation() {
+		return null;
+	}
+
+	public String getDestination() {
+		return null;
+	}
+
+}
