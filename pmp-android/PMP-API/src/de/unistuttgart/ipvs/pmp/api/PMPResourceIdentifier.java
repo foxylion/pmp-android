452bd0562364ac910dd6cbd0053cf89287e7d6af
Tobias Kuhn <kuhnts@studi.informatik.uni-stuttgart.de>
2012-01-17 00:19:06 +0100

    implements PMPResourceIdentifier, refs #386

--- pmp-android/PMP-API/src/de/unistuttgart/ipvs/pmp/api/PMPResourceIdentifier.java
+++ pmp-android/PMP-API/src/de/unistuttgart/ipvs/pmp/api/PMPResourceIdentifier.java
@@ -1,14 +1,47 @@
 package de.unistuttgart.ipvs.pmp.api;
 
+import de.unistuttgart.ipvs.pmp.resource.Resource;
+
+/**
+ * A storage object for the identity of a {@link Resource} in PMP.
+ * 
+ * @author Tobias Kuhn
+ * 
+ */
 public class PMPResourceIdentifier {
-	private String rg;
-	private String res;
-
-	private PMPResourceIdentifier(String rg, String res) {
-		throw new UnsupportedOperationException();
-	}
-
-	public static PMPResourceIdentifier make(String rg, String res) {
-		throw new UnsupportedOperationException();
-	}
+    
+    private final String resourceGroup;
+    private final String resource;
+    
+    
+    private PMPResourceIdentifier(String resourceGroup, String resource) {
+        this.resourceGroup = resourceGroup;
+        this.resource = resource;
+    }
+    
+    
+    protected String getResourceGroup() {
+        return this.resourceGroup;
+    }
+    
+    
+    protected String getResource() {
+        return this.resource;
+    }
+    
+    
+    /**
+     * Creates a new {@link PMPResourceIdentifier} identifying one specific resource throughout PMP. This is useful for
+     * abbreviating calls to the {@link PMP} API. You can store the
+     * returned object for as long as you want, but this is entirely optional.
+     * 
+     * @param resourceGroupPackage
+     *            the package of the resource group you want to access
+     * @param resource
+     *            the identifier of the resource inside the resource group
+     * @return an object that identifies the specific resource throughout PMP
+     */
+    public static PMPResourceIdentifier make(String resourceGroupPackage, String resource) {
+        return new PMPResourceIdentifier(resourceGroupPackage, resource);
+    }
 }
