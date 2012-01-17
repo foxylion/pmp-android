f632ac382f37220e1cd1c828b4193c19bedffdd4
Tobias Kuhn <kuhnts@studi.informatik.uni-stuttgart.de>
2012-01-17 01:32:10 +0100

    implements IPMP, PMP stub, refs #386

--- pmp-android/PMP-API/src/de/unistuttgart/ipvs/pmp/api/PMP.java
+++ pmp-android/PMP-API/src/de/unistuttgart/ipvs/pmp/api/PMP.java
@@ -2,102 +2,301 @@ package de.unistuttgart.ipvs.pmp.api;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentMap;
 
+import android.app.Application;
+import android.os.Bundle;
+import android.os.IBinder;
 import de.unistuttgart.ipvs.pmp.api.handler.PMPRegistrationHandler;
 import de.unistuttgart.ipvs.pmp.api.handler.PMPRequestResourceHandler;
 import de.unistuttgart.ipvs.pmp.api.handler.PMPServiceFeatureUpdateHandler;
 import de.unistuttgart.ipvs.pmp.api.ipc.IPCScheduler;
 
-import android.app.Application;
-import android.os.Bundle;
-import android.os.IBinder;
-
+/**
+ * The main PMP API implementing all the calls.
+ * 
+ * @author Tobias Kuhn
+ * 
+ */
 public class PMP implements IPMP {
     
-    private static PMP instance;
-    private ConcurrentMap<String, Boolean> sfs;
-    private ConcurrentMap<PMPResourceIdentifier, IBinder> res;
+    /**
+     * The static instance of the API
+     */
+    private static PMP instance = null;
+    
+    /**
+     * The {@link Application} for which this API shall perform operations.
+     */
     private Application application;
+    
+    /**
+     * The {@link IPCScheduler} used to communicate with PMP.
+     */
     public IPCScheduler scheduler;
     
+    /**
+     * The cache of Service Feature states.
+     */
+    private ConcurrentMap<String, Boolean> sfs;
     
+    /**
+     * The cache of Resource {@link IBinder}s.
+     */
+    private ConcurrentMap<PMPResourceIdentifier, IBinder> res;
+    
+    
+    /**
+     * Gets the API for an application.
+     * 
+     * @param application
+     *            the {@link Application} for which the API shall perform operations
+     * @return the {@link IPMP} API for the application
+     */
     public static IPMP get(Application application) {
-        throw new UnsupportedOperationException();
+        if (instance == null) {
+            instance = new PMP();
+        }
+        instance.setApplication(application);
+        return instance;
     }
     
     
+    /**
+     * Gets the API, if an API for an application was previously requested. If not, consider calling
+     * {@link PMP#get(Application))} before.
+     * 
+     * @return the {@link IPMP} API for the last application supplied
+     * @throws IllegalAccessError
+     *             if this method is called without ever specifying an {@link Application} via
+     *             {@link PMP#get(Application)}.
+     */
     public static IPMP get() {
-        throw new UnsupportedOperationException();
+        if (instance == null) {
+            throw new IllegalAccessError("Tried to fetch an API without ever specifying an Application.");
+        } else {
+            return instance;
+        }
     }
     
     
+    private void setApplication(Application application) {
+        this.application = application;
+    }
+    
+    
+    /**
+     * Method to be called when service features change, so they can be cached. Can be called from an arbitrary thread.
+     * 
+     * @param update
+     */
     protected void onServiceFeatureUpdate(Bundle update) {
         throw new UnsupportedOperationException();
     }
     
     
-    private void setApplication(Application application) {
-        throw new UnsupportedOperationException();
+    /**
+     * Method to be called when binders are received, so they can be cached. Can be called from an arbitrary thread.
+     * 
+     * @param resource
+     * @param binder
+     */
+    protected void onReceiveResource(PMPResourceIdentifier resource, IBinder binder) {
+        
     }
     
     
+    /*
+     * interface methods
+     */
+    
+    @Override
+    public void register() {
+        // TODO Auto-generated method stub
+        
+    }
+    
+    
+    @Override
+    public void register(PMPRegistrationHandler handler) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+    
+    @Override
+    public void register(PMPRegistrationHandler handler, boolean includeUpdate) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+    
+    @Override
     public void register(PMPRegistrationHandler handler, boolean includeUpdate, int timeout) {
-        throw new UnsupportedOperationException();
+        // TODO Auto-generated method stub
+        
     }
     
     
+    @Override
+    public void updateServiceFeatures() {
+        // TODO Auto-generated method stub
+        
+    }
+    
+    
+    @Override
+    public void updateServiceFeatures(PMPServiceFeatureUpdateHandler handler) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+    
+    @Override
     public void updateServiceFeatures(PMPServiceFeatureUpdateHandler handler, int timeout) {
-        throw new UnsupportedOperationException();
+        // TODO Auto-generated method stub
+        
     }
     
     
-    public void requestServiceFeatures(List<String> sfs, boolean showDialog, int timeout) {
-        throw new UnsupportedOperationException();
+    @Override
+    public void requestServiceFeatures(List<String> serviceFeatures) {
+        // TODO Auto-generated method stub
+        
     }
     
     
+    @Override
+    public void requestServiceFeatures(String... serviceFeatures) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+    
+    @Override
+    public void requestServiceFeatures(List<String> serviceFeatures, boolean showDialog) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+    
+    @Override
+    public void requestServiceFeatures(List<String> serviceFeatures, boolean showDialog, int timeout) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+    
+    @Override
+    public void getResource(PMPResourceIdentifier resource) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+    
+    @Override
+    public void getResource(PMPResourceIdentifier resource, PMPRequestResourceHandler handler) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+    
+    @Override
+    public void getResource(PMPResourceIdentifier resource, PMPRequestResourceHandler handler, int timeout) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+    
+    @Override
     public Map<String, Boolean> getServiceFeatures() {
-        throw new UnsupportedOperationException();
+        // TODO Auto-generated method stub
+        return null;
     }
     
     
-    public boolean isServiceFeatureEnabled(String sf) {
-        throw new UnsupportedOperationException();
+    @Override
+    public boolean isServiceFeatureEnabled(String serviceFeature) {
+        // TODO Auto-generated method stub
+        return false;
     }
     
     
-    public boolean areServiceFeaturesEnabled(List<String> sfs) {
-        throw new UnsupportedOperationException();
+    @Override
+    public boolean areServiceFeaturesEnabled(List<String> serviceFeatures) {
+        // TODO Auto-generated method stub
+        return false;
     }
     
     
-    public List<String> listAvailableServiceFeatures(List<String> sfs) {
-        throw new UnsupportedOperationException();
+    @Override
+    public boolean areServiceFeaturesEnabled(String... serviceFeatures) {
+        // TODO Auto-generated method stub
+        return false;
     }
     
     
-    public List<String> listUnavailableServiceFeatures(List<String> sfs) {
-        throw new UnsupportedOperationException();
+    @Override
+    public boolean areServiceFeaturesDisabled(List<String> serviceFeatures) {
+        // TODO Auto-generated method stub
+        return false;
     }
     
     
-    public List<String> listAllServiceFeatures() {
-        throw new UnsupportedOperationException();
+    @Override
+    public boolean areServiceFeaturesDisabled(String... serviceFeatures) {
+        // TODO Auto-generated method stub
+        return false;
     }
     
     
-    public void getResource(PMPResourceIdentifier res, PMPRequestResourceHandler handler, int timeout) {
-        throw new UnsupportedOperationException();
+    @Override
+    public List<String> listEnabledServiceFeatures(List<String> serviceFeatures) {
+        // TODO Auto-generated method stub
+        return null;
     }
     
     
-    public boolean isResourceCached(PMPResourceIdentifier res) {
-        throw new UnsupportedOperationException();
+    @Override
+    public List<String> listEnabledServiceFeatures(String... serviceFeatures) {
+        // TODO Auto-generated method stub
+        return null;
     }
     
     
-    public IBinder getResourceFromCache(PMPResourceIdentifier res) {
-        throw new UnsupportedOperationException();
+    @Override
+    public List<String> listDisabledServiceFeatures(List<String> serviceFeatures) {
+        // TODO Auto-generated method stub
+        return null;
     }
+    
+    
+    @Override
+    public List<String> listDisabledServiceFeatures(String... serviceFeatures) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+    
+    @Override
+    public Set<String> listAllServiceFeatures() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+    
+    @Override
+    public boolean isResourceCached(PMPResourceIdentifier resource) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+    
+    
+    @Override
+    public IBinder getResourceFromCache(PMPResourceIdentifier resource) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
 }
--- pmp-android/PMP-API/src/de/unistuttgart/ipvs/pmp/api/handler/PMPHandler.java
+++ pmp-android/PMP-API/src/de/unistuttgart/ipvs/pmp/api/handler/PMPHandler.java
@@ -1,16 +1,18 @@
 package de.unistuttgart.ipvs.pmp.api.handler;
 
 public abstract class PMPHandler {
-
-	protected void onPrepare() {
-		throw new UnsupportedOperationException();
-	}
-
-	protected void onTimeout() {
-		throw new UnsupportedOperationException();
-	}
-
-	protected void onFinalize() {
-		throw new UnsupportedOperationException();
-	}
+    
+    protected void onPrepare() {
+        throw new UnsupportedOperationException();
+    }
+    
+    
+    public void onTimeout() {
+        throw new UnsupportedOperationException();
+    }
+    
+    
+    protected void onFinalize() {
+        throw new UnsupportedOperationException();
+    }
 }
