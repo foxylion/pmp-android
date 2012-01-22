4fddec99b5d52d65eaa2f7b58d7c625052b4b7e8
Marcus Vetter <marqus.ve@googlemail.com>
2012-01-22 12:50:47 +0100

    added some comments #refs 404

--- pmp-android/PMP/src/de/unistuttgart/ipvs/pmp/gui/settings/Setting.java
+++ pmp-android/PMP/src/de/unistuttgart/ipvs/pmp/gui/settings/Setting.java
@@ -4,18 +4,34 @@ import android.graphics.drawable.Drawable;
 import de.unistuttgart.ipvs.pmp.gui.util.PMPPreferences;
 
 /**
+ * Simple Setting
  * 
  * @author Marcus Vetter
  * 
  */
 public class Setting {
     
+    /**
+     * Attributes of a Setting
+     */
     private SettingIdentifier identifier;
     private String name;
     private String description;
     private Drawable icon;
     
     
+    /**
+     * Constructor to set the attributes
+     * 
+     * @param identifier
+     *            the identifier of the Setting ({@link SettingIdentifier})
+     * @param name
+     *            name of the Setting
+     * @param description
+     *            description of the Setting
+     * @param icon
+     *            icon of the Setting (drawable)
+     */
     public Setting(SettingIdentifier identifier, String name, String description, Drawable icon) {
         this.setIdentifier(identifier);
         this.setName(name);
@@ -24,46 +40,80 @@ public class Setting {
     }
     
     
+    /**
+     * @return Identifier of the Setting
+     */
     public SettingIdentifier getIdentifier() {
         return identifier;
     }
     
     
+    /**
+     * @param identifier
+     *            identifier of the Setting
+     */
     public void setIdentifier(SettingIdentifier identifier) {
         this.identifier = identifier;
     }
     
     
+    /**
+     * @return name of the Setting
+     */
     public String getName() {
         return name;
     }
     
     
+    /**
+     * @param name
+     *            name of the Setting
+     */
     public void setName(String name) {
         this.name = name;
     }
     
     
+    /**
+     * @return description of the Setting
+     */
     public String getDescription() {
         return description;
     }
     
     
+    /**
+     * @param description
+     *            description of the Setting
+     */
     public void setDescription(String description) {
         this.description = description;
     }
     
     
+    /**
+     * @return icon of the Setting
+     */
     public Drawable getIcon() {
         return icon;
     }
     
     
+    /**
+     * @param icon
+     *            icon of the Setting
+     */
     public void setIcon(Drawable icon) {
         this.icon = icon;
     }
     
     
+    /**
+     * Check, whether the Setting is enabled or not.
+     * The value is returned from the PMPPreferences.
+     * 
+     * @return flag, whether the Setting is enabled or not.
+     */
     public boolean isEnabled() {
         switch (identifier) {
             case EXPERT_MODE:
@@ -75,6 +125,12 @@ public class Setting {
     }
     
     
+    /**
+     * Set the Setting enabled or not. This flag will be stored to the PMPPreferences.
+     * 
+     * @param enabled
+     *            flag
+     */
     public void setEnabled(boolean enabled) {
         switch (identifier) {
             case EXPERT_MODE:
