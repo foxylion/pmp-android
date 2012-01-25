82f74c79ca9e37e9db27458c0c351b1ac5dda829
Tobias Kuhn <kuhnts@studi.informatik.uni-stuttgart.de>
2012-01-25 18:28:21 +0100

    refs #432

---
+++ pmp-android/PMP-API/src/de/unistuttgart/ipvs/pmp/api/handler/TransmissionHandler.java
@@ -0,0 +1,19 @@
+package de.unistuttgart.ipvs.pmp.api.handler;
+
+import android.os.Bundle;
+
+/**
+ * Handler callback for receiving resource group transmissions.
+ * 
+ * @author Tobias Kuhn
+ * 
+ */
+public class TransmissionHandler {
+    
+    /**
+     * Called for each transmission event.
+     */
+    public void onReceiveTransmission(Bundle transmission) {
+    }
+    
+}
