b8b963c3da5e3e5fb0a9379b14ade9f1293abe44
Marcus Vetter <marqus.ve@googlemail.com>
2012-02-03 17:34:14 +0100

    added validValueDescription as attribute and updated ISPrinter.java refs #502

--- pmp-android/PMP-XML-Utilities/src/de/unistuttgart/ipvs/pmp/xmlutil/parser/RGISParser.java
+++ pmp-android/PMP-XML-Utilities/src/de/unistuttgart/ipvs/pmp/xmlutil/parser/RGISParser.java
@@ -20,15 +20,14 @@
 package de.unistuttgart.ipvs.pmp.xmlutil.parser;
 
 import java.io.InputStream;
-import java.util.List;
 
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 
 import de.unistuttgart.ipvs.pmp.xmlutil.common.exception.ParserException;
 import de.unistuttgart.ipvs.pmp.xmlutil.common.exception.ParserException.Type;
-import de.unistuttgart.ipvs.pmp.xmlutil.rgis.RGISPrivacySetting;
 import de.unistuttgart.ipvs.pmp.xmlutil.rgis.RGIS;
+import de.unistuttgart.ipvs.pmp.xmlutil.rgis.RGISPrivacySetting;
 
 /**
  * This XML Parser parses a given xml (for a rg) and creates a rg information
@@ -38,110 +37,113 @@ import de.unistuttgart.ipvs.pmp.xmlutil.rgis.RGIS;
  * 
  */
 public class RGISParser extends AbstractParser {
-    
-    /**
-     * RgInformationSet
-     */
-    private RGIS rgis;    
-    
-    /**
-     * This method parses a given xml (by the xml url) and returns a created
-     * resourcegroup information set
-     * 
-     * @return created rg information set
-     */
-    public RGIS parse(InputStream xmlStream) {
+
+	/**
+	 * RgInformationSet
+	 */
+	private RGIS rgis;
+
+	/**
+	 * This method parses a given xml (by the xml url) and returns a created
+	 * resourcegroup information set
+	 * 
+	 * @return created rg information set
+	 */
+	public RGIS parse(InputStream xmlStream) {
 		// Initialize
 		initParser(xmlStream);
-		
+
 		// Create new RGIS
 		rgis = new RGIS();
-        
-        // Check, if the root node is named correctly
-        if (!this.doc.getDocumentElement().getNodeName().equals("resourceGroupInformationSet")) {
-            throw new ParserException(Type.BAD_ROOT_NODE_NAME, "The name of the root node is invalid.");
-        }
-        
-        // The main nodes "resourceGroupInformation" and "privacySettings" are
-        // required once.
-        NodeList rgInformation = this.doc.getElementsByTagName("resourceGroupInformation");
-        NodeList privacySettings = this.doc.getElementsByTagName("privacySettings");
-        
-        // Check, if there is exactly one resourceGroupInformation and one
-        // privacySettings node
-        if (rgInformation.getLength() < 1) {
-            throw new ParserException(Type.NODE_MISSING, "The node resourceGroupInformation is missing!");
-        } else if (rgInformation.getLength() > 1) {
-            throw new ParserException(Type.NODE_OCCURRED_TOO_OFTEN,
-                    "The node resourceGroupInformation occurred too often!");
-        }
-        if (privacySettings.getLength() < 1) {
-            throw new ParserException(Type.NODE_MISSING, "The node privacySettings is missing!");
-        } else if (privacySettings.getLength() > 1) {
-            throw new ParserException(Type.NODE_OCCURRED_TOO_OFTEN, "The node privacySettings occurred too often!");
-        }
-        
-        // Check, if there are only 2 child nodes of appInformationSet
-        checkNumberOfNodes(2, (Element) this.doc.getElementsByTagName("resourceGroupInformationSet").item(0));
-        
-        // Parse the nodes
-        parseRgInformationNode((Element) rgInformation.item(0));
-        parsePrivacySettingsNode((Element) privacySettings.item(0));
-        
-        return this.rgis;
-    }
-    
-    
-    /**
-     * This method parses the rg information element
-     * 
-     * @param rgInformationElement
-     *            starting with this root element
-     */
-    private void parseRgInformationNode(Element rgInformationElement) {
-    	// Parse names and descriptions
-    	parseNameDescriptionNodes(rgInformationElement, this.rgis);
-    	
-        // Create results and add them to the rg information set
-        this.rgis.setIdentifier(rgInformationElement.getAttribute("identifier"));
-        this.rgis.setIconLocation(rgInformationElement.getAttribute("icon"));
-        this.rgis.setClassName(rgInformationElement.getAttribute("className"));
-    }
-    
-    
-    /**
-     * This method parses the privacy settings element
-     * 
-     * @param privacySettingsElement
-     *            starting with this root element
-     */
-    private void parsePrivacySettingsNode(Element privacySettingsElement) {
-        NodeList privacySettingsNodeList = privacySettingsElement.getElementsByTagName("privacySetting");
-        
-        // Parse the Privacy Settings
-        for (int itr = 0; itr < privacySettingsNodeList.getLength(); itr++) {
-        	// Get the element
-        	Element privacySettingElement = (Element) privacySettingsNodeList.item(itr);
-        	
-        	// Instantiate a new Privacy Setting and add the identifier
-        	RGISPrivacySetting ps = new RGISPrivacySetting(privacySettingElement.getAttribute("identifier"), null);
-            
-        	// Get the valid value description
-            List<String[]> validValueDescrList = parseNodes(privacySettingElement, "validValueDescription");
-            
-            // Check, if the validValueDescription occurres too often or not at all
-            if (validValueDescrList.size() > 1) {
-            	throw new ParserException(Type.NODE_OCCURRED_TOO_OFTEN,
-                        "The node validValueDescription of the Privacy Setting " + ps.getIdentifier() + " occurred too often!");
-            } else if (validValueDescrList.size() == 1) {
-            	ps.setValidValueDescription(validValueDescrList.get(0)[0]);
-            }
-           
-            // Parse names and descriptions
-            parseNameDescriptionNodes(privacySettingElement, ps);
-            
-            // Add the Privacy Setting to the RGIS
-            this.rgis.addPrivacySetting(ps);
-        }
-    }
+
+		// Check, if the root node is named correctly
+		if (!this.doc.getDocumentElement().getNodeName()
+				.equals("resourceGroupInformationSet")) {
+			throw new ParserException(Type.BAD_ROOT_NODE_NAME,
+					"The name of the root node is invalid.");
+		}
+
+		// The main nodes "resourceGroupInformation" and "privacySettings" are
+		// required once.
+		NodeList rgInformation = this.doc
+				.getElementsByTagName("resourceGroupInformation");
+		NodeList privacySettings = this.doc
+				.getElementsByTagName("privacySettings");
+
+		// Check, if there is exactly one resourceGroupInformation and one
+		// privacySettings node
+		if (rgInformation.getLength() < 1) {
+			throw new ParserException(Type.NODE_MISSING,
+					"The node resourceGroupInformation is missing!");
+		} else if (rgInformation.getLength() > 1) {
+			throw new ParserException(Type.NODE_OCCURRED_TOO_OFTEN,
+					"The node resourceGroupInformation occurred too often!");
+		}
+		if (privacySettings.getLength() < 1) {
+			throw new ParserException(Type.NODE_MISSING,
+					"The node privacySettings is missing!");
+		} else if (privacySettings.getLength() > 1) {
+			throw new ParserException(Type.NODE_OCCURRED_TOO_OFTEN,
+					"The node privacySettings occurred too often!");
+		}
+
+		// Check, if there are only 2 child nodes of appInformationSet
+		checkNumberOfNodes(
+				2,
+				(Element) this.doc.getElementsByTagName(
+						"resourceGroupInformationSet").item(0));
+
+		// Parse the nodes
+		parseRgInformationNode((Element) rgInformation.item(0));
+		parsePrivacySettingsNode((Element) privacySettings.item(0));
+
+		return this.rgis;
+	}
+
+	/**
+	 * This method parses the rg information element
+	 * 
+	 * @param rgInformationElement
+	 *            starting with this root element
+	 */
+	private void parseRgInformationNode(Element rgInformationElement) {
+		// Parse names and descriptions
+		parseNameDescriptionNodes(rgInformationElement, this.rgis);
+
+		// Create results and add them to the rg information set
+		this.rgis
+				.setIdentifier(rgInformationElement.getAttribute("identifier"));
+		this.rgis.setIconLocation(rgInformationElement.getAttribute("icon"));
+		this.rgis.setClassName(rgInformationElement.getAttribute("className"));
+	}
+
+	/**
+	 * This method parses the privacy settings element
+	 * 
+	 * @param privacySettingsElement
+	 *            starting with this root element
+	 */
+	private void parsePrivacySettingsNode(Element privacySettingsElement) {
+		NodeList privacySettingsNodeList = privacySettingsElement
+				.getElementsByTagName("privacySetting");
+
+		// Parse the Privacy Settings
+		for (int itr = 0; itr < privacySettingsNodeList.getLength(); itr++) {
+			// Get the element
+			Element privacySettingElement = (Element) privacySettingsNodeList
+					.item(itr);
+
+			// Instantiate a new Privacy Setting and add the identifier and
+			// validValueDescription
+			RGISPrivacySetting ps = new RGISPrivacySetting(
+					privacySettingElement.getAttribute("identifier"),
+					privacySettingElement.getAttribute("validValueDescription"));
+
+			// Parse names and descriptions
+			parseNameDescriptionNodes(privacySettingElement, ps);
+
+			// Add the Privacy Setting to the RGIS
+			this.rgis.addPrivacySetting(ps);
+		}
+	}
 }
