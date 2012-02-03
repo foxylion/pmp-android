f022c0f4049297bbdf23e9c62acb585377e3a05f
Jakob Jarosch <jakob.jarosch@gmail.com>
2012-02-03 09:23:08 +0100

    added some more implementation to the LocationResourceGroup and the test app. refs #429

--- pmp-android/Resources/LocationResourceGroup/src/de/unistuttgart/ipvs/pmp/resourcegroups/location/resource/AbsoluteLocationImpl.java
+++ pmp-android/Resources/LocationResourceGroup/src/de/unistuttgart/ipvs/pmp/resourcegroups/location/resource/AbsoluteLocationImpl.java
@@ -11,6 +11,8 @@ public class AbsoluteLocationImpl extends IAbsoluteLocation.Stub {
 	private AbsoluteLocationResource absoluteLocationR;
 	private String appIdentifier;
 	private PermissionValidator ps;
+	
+	private UpdateRequest updateRequest;
 
 	public AbsoluteLocationImpl(Location locationRG, AbsoluteLocationResource absoluteLocationR, String appIdentifier) {
 		this.locationRG = locationRG;
@@ -20,35 +22,93 @@ public class AbsoluteLocationImpl extends IAbsoluteLocation.Stub {
 		this.ps = new PermissionValidator(this.locationRG,this.appIdentifier);
 	}
 	
-	public void startLocationLookup() throws RemoteException {
+	public void startLocationLookup(long minTime, float minDistance) throws RemoteException {
 		ps.validate(Location.PS_USE_ABSOLUTE_LOCATION, "true");
 		
-		absoluteLocationR.startLocationLookup();
+		updateRequest = new UpdateRequest(minTime, minDistance);
+		absoluteLocationR.startLocationLookup(appIdentifier, updateRequest);
 	}
 
 	public void endLocationLookup() throws RemoteException {
 		ps.validate(Location.PS_USE_ABSOLUTE_LOCATION, "true");
 		
-		absoluteLocationR.endLocationLookup();
+		updateLastRequest();
+		
+		absoluteLocationR.endLocationLookup(this.appIdentifier);
 	}
 
+	public boolean isGpsEnabled() throws RemoteException {
+		ps.validate(Location.PS_USE_ABSOLUTE_LOCATION, "true");
+		
+		updateLastRequest();
+		
+		return this.absoluteLocationR.isGpsEnabled();
+	}
+	
+	public boolean isActive() throws RemoteException {
+		ps.validate(Location.PS_USE_ABSOLUTE_LOCATION, "true");
+		
+		updateLastRequest();
+		
+		return this.absoluteLocationR.isActive();
+	}
+	
 	public boolean isFixed() throws RemoteException {
 		ps.validate(Location.PS_USE_ABSOLUTE_LOCATION, "true");
 		
+		updateLastRequest();
+		
+		return this.absoluteLocationR.isFixed();
+	}
+	
+	public boolean isUpdateAvailable() {
+		ps.validate(Location.PS_USE_ABSOLUTE_LOCATION, "true");
+		
+		updateLastRequest();
+		
+		// TODO implement the is update available
 		return false;
 	}
 
 	public double getLongitude() throws RemoteException {
 		ps.validate(Location.PS_USE_ABSOLUTE_LOCATION, "true");
 		
-		return 0.0;
+		updateLastRequest();
+		
+		// TODO Use the min Detail Privacy Setting as inaccuracy level.
+		return this.absoluteLocationR.getLongitude();
 	}
 
 	public double getLatitude() throws RemoteException {
 		ps.validate(Location.PS_USE_ABSOLUTE_LOCATION, "true");
 		
-		return 0.0;
+		updateLastRequest();
+		
+		// TODO Use the min Detail Privacy Setting as inaccuracy level.
+		return this.absoluteLocationR.getLatitude();
+	}
+
+	public float getAccuracy() throws RemoteException {
+		ps.validate(Location.PS_USE_ABSOLUTE_LOCATION, "true");
+		ps.validate(Location.PS_SHOW_ACCURACY, "true");
+		
+		updateLastRequest();
+
+		// TODO Use the min Detail Privacy Setting as lower inaccuracy level.
+		return this.absoluteLocationR.getAccuracy();
+	}
+
+	public float getSpeed() throws RemoteException {
+		ps.validate(Location.PS_USE_ABSOLUTE_LOCATION, "true");
+		ps.validate(Location.PS_SHOW_SPEED, "true");
+		
+		updateLastRequest();
+		
+		return this.absoluteLocationR.getSpeed();
 	}
 	
-
+	
+	private void updateLastRequest() {
+		this.updateRequest.setLastRequest(System.currentTimeMillis());
+	}
 }
--- pmp-android/Tests/LocationTestApp/src/de/unistuttgart/ipvs/pmp/apps/locationtestapp/gui/LocationAppActivity.java
+++ pmp-android/Tests/LocationTestApp/src/de/unistuttgart/ipvs/pmp/apps/locationtestapp/gui/LocationAppActivity.java
@@ -19,74 +19,225 @@
  */
 package de.unistuttgart.ipvs.pmp.apps.locationtestapp.gui;
 
-import android.app.Activity;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapController;
+import com.google.android.maps.MapView;
+
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.RemoteException;
+import android.text.Html;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 import de.unistuttgart.ipvs.pmp.api.PMP;
 import de.unistuttgart.ipvs.pmp.api.PMPResourceIdentifier;
 import de.unistuttgart.ipvs.pmp.api.handler.PMPRequestResourceHandler;
 import de.unistuttgart.ipvs.pmp.apps.locationtestapp.R;
 import de.unistuttgart.ipvs.pmp.resourcegroups.location.aidl.IAbsoluteLocation;
 
-public class LocationAppActivity extends Activity {
-
-	private static final String RG_NAME = "de.unistuttgart.ipvs.pmp.resourcegroups.location";
-	private static final String R_NAME = "absoluteLocation";
-
-
-	private static final PMPResourceIdentifier R_ID = PMPResourceIdentifier.make(RG_NAME,
-			R_NAME);
-	
-	public Handler handler;
-
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-
-		this.handler = new Handler();
-
-		PMP.get(getApplication());
-
-		setContentView(R.layout.main);
-
-		addListener();
-	}
-
-	@Override
-	protected void onResume() {
-		super.onResume();
-
-		PMP.get().getResource(R_ID, new PMPRequestResourceHandler() {
-			@Override
-			public void onReceiveResource(PMPResourceIdentifier resource,
-					IBinder binder) {
-				resourceCached();
-			}
-			
-			@Override
-			public void onBindingFailed() {
-				Toast.makeText(LocationAppActivity.this, "Binding Resource failed", Toast.LENGTH_LONG).show();
-			}
-		});
-	}
-
-
-	private void resourceCached() {
-		IBinder binder = PMP.get().getResourceFromCache(R_ID);
-		IAbsoluteLocation loc = IAbsoluteLocation.Stub.asInterface(binder);
-		try {
-			loc.startLocationLookup();
-			Toast.makeText(this, "locationFixed: " + loc.isFixed(), Toast.LENGTH_LONG).show();
-		} catch (RemoteException e) {
-			
-		}
-		
-	};
-	
-	private void addListener() {
-		
-	}
+public class LocationAppActivity extends MapActivity {
+    
+    private static final String RG_NAME = "de.unistuttgart.ipvs.pmp.resourcegroups.location";
+    private static final String R_NAME = "absoluteLocation";
+    
+    private static final PMPResourceIdentifier R_ID = PMPResourceIdentifier.make(RG_NAME, R_NAME);
+    
+    public Handler handler;
+    
+    private TimerTask timerTask = new DefaultTimerTask();
+    private Timer timer = null;
+    
+    
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        this.handler = new Handler();
+        
+        PMP.get(getApplication());
+        
+        setContentView(R.layout.main);
+        
+        addListener();
+    }
+    
+    
+    @Override
+    protected void onResume() {
+        super.onResume();
+        
+        PMP.get().getResource(R_ID, new PMPRequestResourceHandler() {
+            
+            @Override
+            public void onReceiveResource(PMPResourceIdentifier resource, IBinder binder) {
+                resourceCached();
+            }
+            
+            
+            @Override
+            public void onBindingFailed() {
+                Toast.makeText(LocationAppActivity.this, "Binding Resource failed", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+    
+    
+    @Override
+    protected void onPause() {
+        super.onPause();
+        
+        IBinder binder = PMP.get().getResourceFromCache(R_ID);
+        
+        if (binder == null) {
+            return;
+        }
+        
+        stopContinousLookup();
+        
+        IAbsoluteLocation loc = IAbsoluteLocation.Stub.asInterface(binder);
+        try {
+            loc.endLocationLookup();
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    
+    private void resourceCached() {
+        IBinder binder = PMP.get().getResourceFromCache(R_ID);
+        IAbsoluteLocation loc = IAbsoluteLocation.Stub.asInterface(binder);
+        try {
+            loc.startLocationLookup(1000, 10.0F);
+            Toast.makeText(this, "Location Resource loaded.", Toast.LENGTH_SHORT).show();
+            
+            startContinousLookup();
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+        
+    };
+    
+    
+    private void startContinousLookup() {
+        timer = new Timer();
+        timer.schedule(timerTask, 1000, 1000);
+    }
+    
+    
+    private void stopContinousLookup() {
+        if (timer != null) {
+            timer.cancel();
+            timer = null;
+        }
+    }
+    
+    
+    private void addListener() {
+        
+    }
+    
+    private class DefaultTimerTask extends TimerTask {
+        
+        @Override
+        public void run() {
+            IBinder binder = PMP.get().getResourceFromCache(R_ID);
+            
+            if (binder == null) {
+                return;
+            }
+            
+            IAbsoluteLocation loc = IAbsoluteLocation.Stub.asInterface(binder);
+            
+            boolean isActive = false;
+            boolean isAvailable = false;
+            boolean isFixed = false;
+            
+            double longitude = 0.0;
+            double latitude = 0.0;
+            float speed = 0.0F;
+            float accuracy = 0.0F;
+            
+            try {
+                try {
+                    isActive = loc.isActive();
+                } catch (SecurityException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    isAvailable = loc.isGpsEnabled();
+                } catch (SecurityException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    isFixed = loc.isFixed();
+                } catch (SecurityException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    longitude = loc.getLongitude();
+                } catch (SecurityException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    latitude = loc.getLatitude();
+                } catch (SecurityException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    speed = loc.getSpeed();
+                } catch (SecurityException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    accuracy = loc.getAccuracy();
+                } catch (SecurityException e) {
+                    e.printStackTrace();
+                }
+                
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+            
+            final boolean isActiveD = isActive;
+            final boolean isAvailableD = isAvailable;
+            final boolean isFixedD = isFixed;
+            
+            final double longitudeD = longitude;
+            final double latitudeD = latitude;
+            final float speedD = speed;
+            final float accuracyD = accuracy;
+            
+            handler.post(new Runnable() {
+                
+                public void run() {
+                    
+                    if (isFixedD) {
+                        MapController controller = ((MapView) findViewById(R.id.MapView)).getController();
+                        controller.animateTo(new GeoPoint(new Double(longitudeD * Math.pow(10, 6)).intValue(),
+                                new Double(longitudeD * Math.pow(10, 6)).intValue()));
+                    }
+                    
+                    ((TextView) findViewById(R.id.TextView_Information)).setText(Html.fromHtml("<html>"
+                            + "<b>Longitude:</b> " + longitudeD + "<br/>" + "<b>Longitude:</b> " + latitudeD + "<br/>"
+                            + "<b>Speed:</b> " + speedD + "<br/>" + "<b>Accuracy:</b> " + accuracyD + "</html>"));
+                    
+                    ((ToggleButton) findViewById(R.id.ToggleButton_Active)).setChecked(isActiveD);
+                    ((ToggleButton) findViewById(R.id.ToggleButton_Avaiable)).setChecked(isAvailableD);
+                    ((ToggleButton) findViewById(R.id.ToggleButton_Fixed)).setChecked(isFixedD);
+                }
+            });
+        }
+    }
+    
+    
+    @Override
+    protected boolean isRouteDisplayed() {
+        return false;
+    }
 }
