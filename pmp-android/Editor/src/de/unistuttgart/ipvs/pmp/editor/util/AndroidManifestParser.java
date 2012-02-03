6ce5df64ed4974d11147c75b4df7f74f2d9b9511
Thorsten Berberich <thorsch.b@googlemail.com>
2012-02-03 17:52:22 +0100

    Added initial editor project closes #506

---
+++ pmp-android/Editor/src/de/unistuttgart/ipvs/pmp/editor/util/AndroidManifestParser.java
@@ -0,0 +1,18 @@
+package de.unistuttgart.ipvs.pmp.editor.util;
+
+import java.io.InputStream;
+
+public class AndroidManifestParser {
+
+	public String getAppIdentifier(InputStream aXmlStream) {
+		throw new UnsupportedOperationException();
+	}
+
+	public void addPMPActivity(InputStream aXmlStream) {
+		throw new UnsupportedOperationException();
+	}
+
+	public boolean isMainActivityExisting() {
+		throw new UnsupportedOperationException();
+	}
+}
