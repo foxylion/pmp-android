412eeb56643392324aeb88a8ba91343da8e45074
Tobias Kuhn <kuhnts@studi.informatik.uni-stuttgart.de>
2012-02-03 19:31:10 +0100

    + RevisionReader in the XML Utils

---
+++ pmp-android/PMP-XML-Utilities/src/de/unistuttgart/ipvs/pmp/xmlutil/revision/RevisionReader.java
@@ -0,0 +1,89 @@
+package de.unistuttgart.ipvs.pmp.xmlutil.revision;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipException;
+import java.util.zip.ZipFile;
+
+/**
+ * Reader to read in the revision date.
+ * 
+ * @author Tobias Kuhn
+ * 
+ */
+public class RevisionReader {
+    
+    /* Singleton stuff */
+    
+    private static final RevisionReader instance = new RevisionReader();
+    
+    
+    private RevisionReader() {
+        
+    }
+    
+    
+    public static RevisionReader get() {
+        return instance;
+    }
+    
+    
+    /* actual implementation */
+    
+    /**
+     * Reads the revision of an apk file.
+     * 
+     * @param apk
+     *            the apk file to read in
+     * @return the revision, i.e. the seconds that have passed since 01.01.1970 = UNIX timestamp,
+     *         or 04:04:04 04.04.1904 if a file wasn't found
+     *         or 23:59:59 31.12.1969 if no time was specified in the APK
+     *         or 06:06:06 06.06.1906 if another error occurred
+     */
+    public long readRevision(File apk) {
+        
+        long result = -2006186034L; // the devil
+        
+        try {
+            ZipFile zipApk = new ZipFile(apk);
+            try {
+                
+                ZipEntry ze = zipApk.getEntry("classes.dex");
+                if (ze == null) {
+                    return -2074708556L; // 404'd
+                }
+                result = Math.max(result, ze.getTime());
+                
+                ze = zipApk.getEntry("assets/rgis.xml");
+                if (ze == null) {
+                    return -2074708556L; // 404'd
+                }
+                result = Math.max(result, ze.getTime());
+                
+            } finally {
+                zipApk.close();
+            }
+            
+        } catch (ZipException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        return result;
+    }
+    
+    
+    /**
+     * 
+     * @param revision
+     *            a revision
+     * @return the revision in human readable form
+     */
+    public String toHumanReadable(long revision) {
+        return SimpleDateFormat.getDateTimeInstance().format(new Date(revision));
+    }
+}
