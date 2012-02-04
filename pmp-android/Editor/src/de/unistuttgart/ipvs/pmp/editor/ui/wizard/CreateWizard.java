5824489ae527a14beb4c6817266d2eb51d908f55
Thorsten Berberich <thorsch.b@googlemail.com>
2012-02-04 11:57:54 +0100

    Added first implementation refs #512

--- pmp-android/Editor/src/de/unistuttgart/ipvs/pmp/editor/ui/wizard/CreateWizard.java
+++ pmp-android/Editor/src/de/unistuttgart/ipvs/pmp/editor/ui/wizard/CreateWizard.java
@@ -1,11 +1,24 @@
 package de.unistuttgart.ipvs.pmp.editor.ui.wizard;
 
 import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.ui.INewWizard;
 import org.eclipse.ui.IWorkbench;
+
+import de.unistuttgart.ipvs.pmp.editor.util.AndroidManifestAdapter;
+import de.unistuttgart.ipvs.pmp.editor.util.AndroidManifestParser;
 
 public class CreateWizard extends Wizard implements INewWizard {
     private WizardPageCreate page;
@@ -19,8 +32,8 @@ public class CreateWizard extends Wizard implements INewWizard {
     }
 
     @Override
-    public void init(IWorkbench aWorkbench, IStructuredSelection aSelection) {
-	throw new UnsupportedOperationException();
+    public void init(IWorkbench Workbench, IStructuredSelection Selection) {
+
     }
 
     @Override
--- pmp-android/Editor/src/de/unistuttgart/ipvs/pmp/editor/util/AndroidManifestAdapter.java
+++ pmp-android/Editor/src/de/unistuttgart/ipvs/pmp/editor/util/AndroidManifestAdapter.java
@@ -1,16 +1,62 @@
 package de.unistuttgart.ipvs.pmp.editor.util;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.Path;
+
 public class AndroidManifestAdapter {
 
-	public String getAppIdentifier(String aXmlPath) {
-		throw new UnsupportedOperationException();
-	}
+    /**
+     * Calls the {@link AndroidManifestAdapter} to get the app identifier
+     * 
+     * @param xmlPath
+     *            path to the XML file in the workspace (e.g. "/myProject/")
+     * @param xmlFile
+     *            file name (should be AndroidManifest.xml)
+     * @return the app identifier
+     * @throws FileNotFoundException
+     *             thrown if the AndroidManifest.xml is not found
+     */
+    public String getAppIdentifier(String xmlPath, String xmlFile)
+	    throws FileNotFoundException {
+	return new AndroidManifestParser().getAppIdentifier(getInputStream(
+		xmlPath, xmlFile));
+    }
 
-	public void addPMPActivityToManifest(String aXmlPath) {
-		throw new UnsupportedOperationException();
-	}
+    public void addPMPActivityToManifest(String xmlPath, String xmlFile) {
+	throw new UnsupportedOperationException();
+    }
 
-	public boolean isMainActivityExisting() {
-		throw new UnsupportedOperationException();
-	}
+    public boolean isMainActivityExisting() {
+	throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Gets an {@link InputStream} to an path and a file name
+     * 
+     * @param xmlPath
+     *            path to the file
+     * @param xmlFile
+     *            file name
+     * @return {@link InputStream} to the file
+     * @throws FileNotFoundException
+     *             thrown when the file is not found
+     */
+    private InputStream getInputStream(String xmlPath, String xmlFile)
+	    throws FileNotFoundException {
+	IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
+	IResource resource = root.findMember(new Path(xmlPath));
+	IContainer container = (IContainer) resource;
+	IFile file = container.getFile(new Path(xmlFile));
+	InputStream xmlInputStream = new FileInputStream(file.getLocation()
+		.toOSString());
+	return xmlInputStream;
+    }
 }
