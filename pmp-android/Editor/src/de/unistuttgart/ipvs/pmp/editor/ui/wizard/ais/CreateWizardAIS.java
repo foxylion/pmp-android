ef4c7e470e06941a18058e4b03436d2f60af33b1
Thorsten Berberich <thorsch.b@googlemail.com>
2012-02-07 23:03:37 +0100

    tried to create the file refs #535

--- pmp-android/Editor/src/de/unistuttgart/ipvs/pmp/editor/ui/wizard/ais/CreateWizardAIS.java
+++ pmp-android/Editor/src/de/unistuttgart/ipvs/pmp/editor/ui/wizard/ais/CreateWizardAIS.java
@@ -1,12 +1,34 @@
 package de.unistuttgart.ipvs.pmp.editor.ui.wizard.ais;
 
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.InvocationTargetException;
 
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.ui.INewWizard;
 import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.ide.IDE;
+
+import de.unistuttgart.ipvs.pmp.xmlutil.ais.AIS;
+import de.unistuttgart.ipvs.pmp.xmlutil.compiler.AISCompiler;
 
 public class CreateWizardAIS extends Wizard implements INewWizard {
     private WizardPageCreateAIS page;
@@ -33,16 +55,86 @@ public class CreateWizardAIS extends Wizard implements INewWizard {
 
     @Override
     public boolean performFinish() {
-	// TODO Auto-generated method stub
-	return false;
+	final String containerName = page.getProjectName();
+	final String fileName = "ais.xml";
+	IRunnableWithProgress op = new IRunnableWithProgress() {
+	    public void run(IProgressMonitor monitor)
+		    throws InvocationTargetException {
+		try {
+		    doFinish(containerName, fileName, monitor);
+		} catch (CoreException e) {
+		    throw new InvocationTargetException(e);
+		} finally {
+		    monitor.done();
+		}
+	    }
+	};
+	try {
+	    getContainer().run(true, false, op);
+	} catch (InterruptedException e) {
+	    return false;
+	} catch (InvocationTargetException e) {
+	    Throwable realException = e.getTargetException();
+	    MessageDialog.openError(getShell(), "Error",
+		    realException.getMessage());
+	    return false;
+	}
+	return true;
     }
 
-    private void writeFile() {
-	throw new UnsupportedOperationException();
+    private void doFinish(String containerName, String fileName,
+	    IProgressMonitor monitor) throws CoreException {
+
+	// Split only the project out of the string
+	String[] projects = containerName.split("/");
+
+	// The project name
+	String project = "";
+	if (projects.length >= 2) {
+	    project = "/" + projects[1];
+	}
+
+	monitor.beginTask("Creating ais.xml", 2);
+	IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
+	IResource resource = root.findMember(new Path(containerName));
+	if (!resource.exists() || !(resource instanceof IContainer)) {
+	    throwCoreException("Container \"" + project + "\" does not exist.");
+
+	    AIS ais = new AIS();
+	    IContainer container = (IContainer) resource;
+	    final IFile file = container.getFile(new Path("/assets/" + fileName));
+	    try {
+//		InputStream stream = new AISCompiler().compile(ais);
+		 InputStream stream = new
+		 ByteArrayInputStream("abc".getBytes());
+		if (file.exists()) {
+		    file.setContents(stream, true, true, monitor);
+		} else {
+		    file.create(stream, true, monitor);
+		}
+		stream.close();
+	    } catch (IOException e) {
+	    }
+	    monitor.worked(1);
+	    monitor.setTaskName("Opening file for editing...");
+	    getShell().getDisplay().asyncExec(new Runnable() {
+		public void run() {
+		    IWorkbenchPage page = PlatformUI.getWorkbench()
+			    .getActiveWorkbenchWindow().getActivePage();
+		    try {
+			IDE.openEditor(page, file, true);
+		    } catch (PartInitException e) {
+		    }
+		}
+	    });
+	    monitor.worked(1);
+	}
     }
 
-    private ByteArrayInputStream getContentStream() {
-	throw new UnsupportedOperationException();
+    private void throwCoreException(String message) throws CoreException {
+	IStatus status = new Status(IStatus.ERROR,
+		"Editor for App-Information-Sets", IStatus.OK, message, null);
+	throw new CoreException(status);
     }
 
 }
