0e18632dd00366ff244b729a6d04b58722cb8748
Patrick Strobel <patrick.strobel.swt@gmail.com>
2012-02-07 18:46:29 +0100

    Added sub-pages to editor. Refs #541

---
+++ pmp-android/Editor/src/de/unistuttgart/ipvs/pmp/editor/ui/editors/rgis/PrivacySettingsPage.java
@@ -0,0 +1,15 @@
+package de.unistuttgart.ipvs.pmp.editor.ui.editors.rgis;
+
+import org.eclipse.ui.forms.editor.FormEditor;
+import org.eclipse.ui.forms.editor.FormPage;
+
+public class PrivacySettingsPage extends FormPage {
+
+	public PrivacySettingsPage(FormEditor parent) {
+		super(parent, "Privacy Settings", "PS Tab");
+		// TODO Auto-generated constructor stub
+	}
+
+
+
+}
