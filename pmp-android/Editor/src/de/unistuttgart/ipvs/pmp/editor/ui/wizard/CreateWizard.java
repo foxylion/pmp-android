acebf4dfea5c2e5d13b228c3782f53b19b2bae18
Thorsten Berberich <thorsch.b@googlemail.com>
2012-02-07 13:53:59 +0100

    Fixede wrong imports

--- pmp-android/Editor/src/de/unistuttgart/ipvs/pmp/editor/ui/wizard/CreateWizard.java
+++ pmp-android/Editor/src/de/unistuttgart/ipvs/pmp/editor/ui/wizard/CreateWizard.java
@@ -1,32 +1,10 @@
 package de.unistuttgart.ipvs.pmp.editor.ui.wizard;
 
 import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactoryConfigurationError;
-
-import org.eclipse.core.resources.IContainer;
-import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IResource;
-import org.eclipse.core.resources.IWorkspaceRoot;
-import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.Path;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.ui.INewWizard;
 import org.eclipse.ui.IWorkbench;
-import org.xml.sax.SAXException;
-
-import de.unistuttgart.ipvs.pmp.editor.exceptions.AndroidApplicationException;
-import de.unistuttgart.ipvs.pmp.editor.exceptions.PMPActivityAlreadyExistsException;
-import de.unistuttgart.ipvs.pmp.editor.util.AndroidManifestAdapter;
-import de.unistuttgart.ipvs.pmp.editor.util.AndroidManifestParser;
 
 public class CreateWizard extends Wizard implements INewWizard {
     private WizardPageCreate page;
