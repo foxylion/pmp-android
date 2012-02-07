a7660d104d12aafe8ff6ddc1876ea8570dd57939
Tobias Kuhn <kuhnts@studi.informatik.uni-stuttgart.de>
2012-02-07 10:24:22 +0100

    FindBugs for JPMPPS-CommunicationLib refs #479

--- pmp-android/Webservices/JPMPPS-CommunicationLib/src/de/unistuttgart/ipvs/pmp/jpmpps/io/response/NoSuchPackageResponse.java
+++ pmp-android/Webservices/JPMPPS-CommunicationLib/src/de/unistuttgart/ipvs/pmp/jpmpps/io/response/NoSuchPackageResponse.java
@@ -1,7 +1,7 @@
 package de.unistuttgart.ipvs.pmp.jpmpps.io.response;
 
 public class NoSuchPackageResponse extends AbstractResponse {
-
-	private static final long serialVersionUID = 2L;
-
+    
+    private static final long serialVersionUID = 2L;
+    
 }
