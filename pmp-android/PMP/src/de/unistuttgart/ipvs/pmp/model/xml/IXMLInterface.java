4a01c82b9b035e2dd127a38468b4d8036d8cde8d
Tobias Kuhn <kuhnts@studi.informatik.uni-stuttgart.de>
2012-02-08 13:21:47 +0100

    fixes #543

---
+++ pmp-android/PMP/src/de/unistuttgart/ipvs/pmp/model/xml/IXMLInterface.java
@@ -0,0 +1,34 @@
+package de.unistuttgart.ipvs.pmp.model.xml;
+
+import de.unistuttgart.ipvs.pmp.model.Model;
+import de.unistuttgart.ipvs.pmp.model.element.preset.IPreset;
+import de.unistuttgart.ipvs.pmp.xmlutil.presetset.Preset;
+import de.unistuttgart.ipvs.pmp.xmlutil.presetset.PresetSet;
+
+/**
+ * Interface for the {@link Model} to the PMP-XML-UTILITIES
+ * 
+ * @author Tobias Kuhn
+ * 
+ */
+public interface IXMLInterface {
+    
+    /**
+     * Exports the selected presets into an {@link IPreset}.
+     * 
+     * @param presets
+     * @return
+     */
+    public PresetSet exportPresets(IPreset[] presets);
+    
+    
+    /**
+     * Imports the selected presets into the Model.
+     * 
+     * @param presets
+     * @param override
+     *            if existing presets shall be overridden or a new preset shall be generated
+     */
+    public void importPresets(Preset[] presets, boolean override) throws InvalidPresetSetException;
+    
+}
