85e0e644810162518031bba1e7be4c1eeffba8c4
Alexander Wassiljew <alexanderwassiljew@googlemail.com>
2012-02-09 14:00:03 +0100

    Complete Communication refs #438

---
+++ pmp-android/Apps/vHike/src/de/unistuttgart/ipvs/pmp/apps/vhike/model/SliderObject.java
@@ -0,0 +1,17 @@
+package de.unistuttgart.ipvs.pmp.apps.vhike.model;
+
+
+public class SliderObject {
+    
+    private FoundProfilePos object;
+    public SliderObject(FoundProfilePos object) {
+        this.object = object;
+    }
+    
+    public FoundProfilePos getObject() {
+        return object;
+    }
+    
+    
+    
+}
