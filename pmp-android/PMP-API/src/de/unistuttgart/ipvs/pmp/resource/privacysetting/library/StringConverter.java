6a05fc30539bad04576897663d003cf30c31c0de
Tobias Kuhn <kuhnts@studi.informatik.uni-stuttgart.de>
2012-02-15 13:11:29 +0100

    forgotten file

---
+++ pmp-android/PMP-API/src/de/unistuttgart/ipvs/pmp/resource/privacysetting/library/StringConverter.java
@@ -0,0 +1,16 @@
+package de.unistuttgart.ipvs.pmp.resource.privacysetting.library;
+
+/**
+ * Interface for a converter for a type from string and to string.
+ * 
+ * @author Tobias Kuhn
+ * 
+ */
+public interface StringConverter<T> {
+    
+    public T valueOf(String string);
+    
+    
+    public String toString(T value);
+    
+}
