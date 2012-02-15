d5b72e06668317c1d9cd97117cbe4bcc4391a903
Jakob Jarosch <jakob.jarosch@gmail.com>
2012-02-15 09:58:01 +0100

    made a quick and dirty implementation of SetPrivacySetting for testing purposes. refs #567

--- pmp-android/PMP-API/src/de/unistuttgart/ipvs/pmp/resource/privacysetting/SetPrivacySetting.java
+++ pmp-android/PMP-API/src/de/unistuttgart/ipvs/pmp/resource/privacysetting/SetPrivacySetting.java
@@ -1,6 +1,10 @@
 package de.unistuttgart.ipvs.pmp.resource.privacysetting;
 
 import android.content.Context;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class SetPrivacySetting<T> extends DefaultPrivacySetting {
     
@@ -13,23 +17,89 @@ public class SetPrivacySetting<T> extends DefaultPrivacySetting {
     
     
     @Override
+    public boolean permits(String value, String reference) throws PrivacySettingValueException {
+        String[] values = parseValue(value);
+        String[] references = parseValue(reference);
+        
+        for (String r : references) {
+            boolean found = false;
+            for (String v : values) {
+                if (v.equals(r)) {
+                    found = true;
+                    break;
+                }
+            }
+            
+            if (!found) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+    
+    
+    @Override
     public String getHumanReadableValue(String value) throws PrivacySettingValueException {
-        // TODO Auto-generated method stub
-        return null;
+        return value.toString();
     }
     
     
     @Override
-    public T parseValue(String value) throws PrivacySettingValueException {
-        // TODO Auto-generated method stub
-        return null;
+    public String[] parseValue(String value) throws PrivacySettingValueException {
+        return value.split(separator.toString());
     }
     
     
     @Override
-    public IPrivacySettingView getView(Context context) {
-        // TODO Auto-generated method stub
-        return null;
+    public IPrivacySettingView<String> getView(Context context) {
+        return new SetPrivacySettingView(context, separator.toString());
+    }
+    
+}
+
+/**
+ * {@link IPrivacySettingView} for {@link IntegerPrivacySetting}
+ * 
+ * @author Jakob Jarosch
+ * 
+ */
+class SetPrivacySettingView extends LinearLayout implements IPrivacySettingView<String> {
+    
+    private EditText editText;
+    
+    
+    public SetPrivacySettingView(Context context, String separator) {
+        super(context);
+        
+        setOrientation(LinearLayout.VERTICAL);
+        
+        TextView description = new TextView(context);
+        description.setText("Enter the entries separated by '" + separator + "'.");
+        addView(description);
+        
+        this.editText = new EditText(context);
+        this.editText.setLayoutParams(new LayoutParams(android.view.ViewGroup.LayoutParams.FILL_PARENT,
+                android.view.ViewGroup.LayoutParams.WRAP_CONTENT));
+        addView(this.editText);
+    }
+    
+    
+    @Override
+    public View asView() {
+        return this;
+    }
+    
+    
+    @Override
+    public void setViewValue(String value) throws PrivacySettingValueException {
+        this.editText.setText(value);
+    }
+    
+    
+    @Override
+    public String getViewValue() {
+        return this.editText.getText().toString();
     }
     
 }
