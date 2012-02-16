8411af8d8a0d0c38542eb98465045a30a631921d
Thorsten Berberich <thorsch.b@googlemail.com>
2012-02-16 22:21:42 +0100

    added some commentaries refs #509

--- pmp-android/Editor/src/de/unistuttgart/ipvs/pmp/editor/ui/editors/internals/ais/DescriptionContentProvider.java
+++ pmp-android/Editor/src/de/unistuttgart/ipvs/pmp/editor/ui/editors/internals/ais/DescriptionContentProvider.java
@@ -1,4 +1,5 @@
 package de.unistuttgart.ipvs.pmp.editor.ui.editors.internals.ais;
+
 import org.eclipse.jface.viewers.IContentProvider;
 import org.eclipse.jface.viewers.IStructuredContentProvider;
 import org.eclipse.jface.viewers.Viewer;
@@ -14,23 +15,33 @@ import de.unistuttgart.ipvs.pmp.xmlutil.ais.AISServiceFeature;
 public class DescriptionContentProvider implements IContentProvider,
 	IStructuredContentProvider {
 
-    /* (non-Javadoc)
-     * @see org.eclipse.jface.viewers.IStructuredContentProvider#getElements(java.lang.Object)
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * org.eclipse.jface.viewers.IStructuredContentProvider#getElements(java
+     * .lang.Object)
      */
     @Override
     public Object[] getElements(Object arg0) {
-	return ((AISServiceFeature)arg0).getDescriptions().toArray();
+	return ((AISServiceFeature) arg0).getDescriptions().toArray();
     }
 
-    /* (non-Javadoc)
+    /*
+     * (non-Javadoc)
+     * 
      * @see org.eclipse.jface.viewers.IContentProvider#dispose()
      */
     @Override
-    public void dispose() {	
+    public void dispose() {
     }
 
-    /* (non-Javadoc)
-     * @see org.eclipse.jface.viewers.IContentProvider#inputChanged(org.eclipse.jface.viewers.Viewer, java.lang.Object, java.lang.Object)
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * org.eclipse.jface.viewers.IContentProvider#inputChanged(org.eclipse.jface
+     * .viewers.Viewer, java.lang.Object, java.lang.Object)
      */
     @Override
     public void inputChanged(Viewer arg0, Object arg1, Object arg2) {
