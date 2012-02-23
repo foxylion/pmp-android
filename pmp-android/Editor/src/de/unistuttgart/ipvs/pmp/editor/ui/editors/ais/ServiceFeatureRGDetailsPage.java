9af07233f7ea19eed0242a3dd9d6728c24677b27
Thorsten Berberich <thorsch.b@googlemail.com>
2012-02-23 20:03:23 +0100

    removed the section toolbar from the ps page refs #635

--- pmp-android/Editor/src/de/unistuttgart/ipvs/pmp/editor/ui/editors/ais/ServiceFeatureRGDetailsPage.java
+++ pmp-android/Editor/src/de/unistuttgart/ipvs/pmp/editor/ui/editors/ais/ServiceFeatureRGDetailsPage.java
@@ -1,10 +1,9 @@
 package de.unistuttgart.ipvs.pmp.editor.ui.editors.ais;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-import org.eclipse.jface.action.Action;
-import org.eclipse.jface.action.ToolBarManager;
 import org.eclipse.jface.dialogs.InputDialog;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ColumnLabelProvider;
@@ -17,19 +16,18 @@ import org.eclipse.jface.viewers.TreePath;
 import org.eclipse.jface.viewers.TreeSelection;
 import org.eclipse.jface.window.Window;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.DisposeEvent;
-import org.eclipse.swt.events.DisposeListener;
-import org.eclipse.swt.graphics.Cursor;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableColumn;
 import org.eclipse.swt.widgets.TableItem;
-import org.eclipse.swt.widgets.ToolBar;
 import org.eclipse.ui.dialogs.SelectionDialog;
 import org.eclipse.ui.forms.IDetailsPage;
 import org.eclipse.ui.forms.IFormPart;
@@ -59,7 +57,7 @@ import de.unistuttgart.ipvs.pmp.xmlutil.rgis.RGIS;
  * 
  */
 public class ServiceFeatureRGDetailsPage implements IDetailsPage,
-	IDoubleClickListener {
+	IDoubleClickListener, SelectionListener {
 
     /**
      * ID of this page
@@ -85,6 +83,11 @@ public class ServiceFeatureRGDetailsPage implements IDetailsPage,
      * Required privacy setting {@link TableViewer}
      */
     private TableViewer psTableViewer;
+
+    /**
+     * The remove button
+     */
+    private Button removeButton;
 
     /**
      * Columns of the table
@@ -136,13 +139,23 @@ public class ServiceFeatureRGDetailsPage implements IDetailsPage,
 	psSection.setLayout(new GridLayout(1, false));
 	psSection.setExpanded(true);
 	psSection.setLayoutData(parentLayout);
-	createPrivacySettingSectionAttributeToolbar(psSection);
 
 	// Composite that is display in the description section
 	Composite psComposite = toolkit.createComposite(psSection);
-	psComposite.setLayout(new GridLayout(1, false));
+	psComposite.setLayout(new GridLayout(2, false));
 	psComposite.setLayoutData(parentLayout);
 	createRGTable(psComposite, toolkit);
+
+	Composite psButtonsComp = toolkit.createComposite(psComposite);
+	psButtonsComp.setLayout(new FillLayout(SWT.VERTICAL));
+	GridData buttonLayout = new GridData();
+	buttonLayout.verticalAlignment = SWT.BEGINNING;
+	psButtonsComp.setLayoutData(buttonLayout);
+	Button addButton = toolkit.createButton(psButtonsComp, "Add", SWT.PUSH);
+	addButton.addSelectionListener(this);
+	removeButton = toolkit.createButton(psButtonsComp, "Remove", SWT.PUSH);
+	removeButton.addSelectionListener((SelectionListener) this);
+	removeButton.setEnabled(false);
 
 	psSection.setClient(psComposite);
     }
@@ -218,6 +231,23 @@ public class ServiceFeatureRGDetailsPage implements IDetailsPage,
 	psTable.setHeaderVisible(true);
 	psTable.setLinesVisible(true);
 
+	// SelectionListener to enable and disable the remove button
+	psTableViewer.getTable().addSelectionListener(new SelectionListener() {
+
+	    @Override
+	    public void widgetSelected(SelectionEvent arg0) {
+		if (psTableViewer.getTable().getSelectionCount() > 0) {
+		    removeButton.setEnabled(true);
+		} else {
+		    removeButton.setEnabled(false);
+		}
+	    }
+
+	    @Override
+	    public void widgetDefaultSelected(SelectionEvent arg0) {
+	    }
+	});
+
 	return psTableViewer;
     }
 
@@ -237,6 +267,8 @@ public class ServiceFeatureRGDetailsPage implements IDetailsPage,
 	displayed = (AISRequiredResourceGroup) path[0].getLastSegment();
 	psTableViewer.setInput(displayed);
 
+	removeButton.setEnabled(false);
+
 	// Pack all columns
 	identifierColumn.pack();
 	valueColumn.pack();
@@ -253,190 +285,6 @@ public class ServiceFeatureRGDetailsPage implements IDetailsPage,
     @Override
     public boolean isDirty() {
 	return model.isAisDirty();
-    }
-
-    /**
-     * Adds the toolbar with the remove and add buttons to the description
-     * section
-     * 
-     * @param section
-     *            {@link Section} to set the toolbar
-     */
-    private void createPrivacySettingSectionAttributeToolbar(Section section) {
-	// Create the toolbar
-	ToolBarManager toolBarManager = new ToolBarManager(SWT.FLAT);
-	ToolBar toolbar = toolBarManager.createControl(section);
-
-	final Cursor handCursor = new Cursor(Display.getCurrent(),
-		SWT.CURSOR_HAND);
-	toolbar.setCursor(handCursor);
-	toolbar.addDisposeListener(new DisposeListener() {
-	    @Override
-	    public void widgetDisposed(DisposeEvent e) {
-		if ((handCursor != null) && (handCursor.isDisposed() == false)) {
-		    handCursor.dispose();
-		}
-	    }
-	});
-
-	// Picture can be added also to the actions
-	Action add = new Action("Add") {
-
-	    @Override
-	    public void run() {
-		// Flag if an error happend while downloading
-		Boolean error = false;
-		RGIS resGroup = null;
-
-		// Get the resource groups from the server
-		List<RGIS> rgList = model.getRgisList(parentShell);
-		if (rgList != null) {
-		    for (RGIS rgis : rgList) {
-			if (rgis.getIdentifier().equals(
-				displayed.getIdentifier())) {
-			    resGroup = rgis;
-			}
-		    }
-		} else {
-		    error = true;
-		}
-
-		// Build a custom RGIS with the privacy settings that are
-		// not set yet
-		RGIS customRGIS = new RGIS();
-
-		// Check if there are RGs from the server
-		if (resGroup != null) {
-		    HashMap<String, IRGISPrivacySetting> privacySettings = new HashMap<String, IRGISPrivacySetting>();
-
-		    // Iterate through all available PSs
-		    for (IRGISPrivacySetting ps : resGroup.getPrivacySettings()) {
-			privacySettings.put(ps.getIdentifier(), ps);
-		    }
-
-		    /*
-		     * Iterate through the set of required Privacy Settings and
-		     * delete the ones that are already added
-		     */
-		    for (IAISRequiredPrivacySetting requiredPS : displayed
-			    .getRequiredPrivacySettings()) {
-
-			for (IRGISPrivacySetting ps : resGroup
-				.getPrivacySettings()) {
-			    if (!ps.getIdentifier().equals(
-				    requiredPS.getIdentifier())) {
-				if (!customRGIS.getPrivacySettings().contains(
-					ps)) {
-				    customRGIS.addPrivacySetting(ps);
-				}
-			    }
-			}
-		    }
-
-		    // If there are some PS to add
-		    if (!customRGIS.getPrivacySettings().isEmpty()) {
-
-			// Open the dialog
-			SelectionDialog dialog = new RequiredPrivacySettingsDialog(
-				parentShell, customRGIS);
-
-			// Get the results
-			if (dialog.open() == Window.OK
-				&& dialog.getResult().length > 0) {
-
-			    // Store them at the model
-			    for (Object object : dialog.getResult()) {
-				AISRequiredPrivacySetting rps = (AISRequiredPrivacySetting) object;
-				displayed.addRequiredPrivacySetting(rps);
-
-				AISValidatorWrapper.getInstance()
-					.validateRequiredPrivacySetting(rps,
-						true);
-			    }
-
-			    AISValidatorWrapper.getInstance()
-				    .validateRequiredResourceGroup(displayed,
-					    true);
-			    AISValidatorWrapper.getInstance()
-				    .validateServiceFeatures(model.getAis(),
-					    true);
-
-			    ServiceFeatureMasterBlock.refreshTree();
-			    // Update the view
-			    psTableViewer.refresh();
-			    psTableViewer.getTable().setRedraw(false);
-			    identifierColumn.pack();
-			    valueColumn.pack();
-			    psTableViewer.getTable().setRedraw(true);
-			    psTableViewer.getTable().redraw();
-			    model.setAISDirty(true);
-			}
-
-			// There are no PS to add to this service feature
-		    } else {
-			MessageDialog
-				.openInformation(parentShell,
-					"No Privacy Settings to add",
-					"You already added all Privacy Settings of this Resource Group");
-		    }
-		} else {
-		    /*
-		     * The Resource group wasn't found at the server and no
-		     * error happen while downloading them, show the
-		     * corresponding message
-		     */
-		    if (!error) {
-			MessageDialog
-				.openInformation(
-					parentShell,
-					"Unknown Resource Group",
-					"This Resource Group was not found at the Resource Group server.\n"
-						+ "Therefore you can not add a Privacy Setting");
-		    }
-		}
-
-	    }
-	};
-	add.setToolTipText("Add a new required Privacy Setting for the Service Feature");
-
-	// The remove action
-	Action remove = new Action("Remove") {
-
-	    @Override
-	    public void run() {
-		TableItem[] selected = psTableViewer.getTable().getSelection();
-		for (TableItem item : selected) {
-		    AISRequiredPrivacySetting ps = (AISRequiredPrivacySetting) item
-			    .getData();
-		    displayed.removeRequiredPrivacySetting(ps);
-		}
-
-		AISValidatorWrapper.getInstance()
-			.validateRequiredResourceGroup(displayed, true);
-		AISValidatorWrapper.getInstance().validateServiceFeatures(
-			model.getAis(), true);
-
-		ServiceFeatureMasterBlock.refreshTree();
-
-		// Update the view
-		psTableViewer.refresh();
-		psTableViewer.getTable().setRedraw(false);
-		identifierColumn.pack();
-		valueColumn.pack();
-		psTableViewer.getTable().redraw();
-		psTableViewer.getTable().setRedraw(true);
-
-		model.setAISDirty(true);
-	    }
-	};
-	remove.setToolTipText("Remove the selected required Privacy Setting");
-
-	// Add the actions to the toolbar
-	toolBarManager.add(add);
-	toolBarManager.add(remove);
-
-	toolBarManager.update(true);
-	section.setTextClient(toolbar);
     }
 
     /*
@@ -578,5 +426,182 @@ public class ServiceFeatureRGDetailsPage implements IDetailsPage,
     @Override
     public boolean setFormInput(Object arg0) {
 	return false;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * org.eclipse.swt.events.SelectionListener#widgetDefaultSelected(org.eclipse
+     * .swt.events.SelectionEvent)
+     */
+    @Override
+    public void widgetDefaultSelected(SelectionEvent arg0) {
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * org.eclipse.swt.events.SelectionListener#widgetSelected(org.eclipse.swt
+     * .events.SelectionEvent)
+     */
+    @Override
+    public void widgetSelected(SelectionEvent widget) {
+	if (widget.getSource() instanceof Button) {
+	    Button clicked = (Button) widget.getSource();
+
+	    // Remove the selected items
+	    if (clicked.getText().equals("Remove")) {
+		TableItem[] selected = psTableViewer.getTable().getSelection();
+		for (TableItem item : selected) {
+		    AISRequiredPrivacySetting ps = (AISRequiredPrivacySetting) item
+			    .getData();
+		    displayed.removeRequiredPrivacySetting(ps);
+		}
+
+		AISValidatorWrapper.getInstance()
+			.validateRequiredResourceGroup(displayed, true);
+		AISValidatorWrapper.getInstance().validateServiceFeatures(
+			model.getAis(), true);
+
+		ServiceFeatureMasterBlock.refreshTree();
+
+		// Update the view
+		psTableViewer.refresh();
+		psTableViewer.getTable().setRedraw(false);
+		identifierColumn.pack();
+		valueColumn.pack();
+		psTableViewer.getTable().redraw();
+		psTableViewer.getTable().setRedraw(true);
+
+		model.setAISDirty(true);
+	    }
+
+	    if (clicked.getText().equals("Add")) {
+		// Flag if an error happend while downloading
+		Boolean error = false;
+		RGIS resGroup = null;
+
+		// Get the resource groups from the server
+		List<RGIS> rgList = model.getRgisList(parentShell);
+		if (rgList != null) {
+		    for (RGIS rgis : rgList) {
+			if (rgis.getIdentifier().equals(
+				displayed.getIdentifier())) {
+			    resGroup = rgis;
+			}
+		    }
+		} else {
+		    error = true;
+		}
+
+		// Build a custom RGIS with the privacy settings that are
+		// not set yet
+		RGIS customRGIS = new RGIS();
+
+		// Check if there are RGs from the server
+		if (resGroup != null) {
+		    HashMap<String, IRGISPrivacySetting> privacySettings = new HashMap<String, IRGISPrivacySetting>();
+
+		    // Iterate through all available PSs
+		    for (IRGISPrivacySetting ps : resGroup.getPrivacySettings()) {
+			privacySettings.put(ps.getIdentifier(), ps);
+		    }
+
+		    // Build a list with all added privacy settings
+		    ArrayList<String> addedIdentifiers = new ArrayList<String>();
+		    for (IAISRequiredPrivacySetting ps : displayed
+			    .getRequiredPrivacySettings()) {
+			addedIdentifiers.add(ps.getIdentifier());
+		    }
+
+		    /*
+		     * Iterate through the set of required Privacy Settings
+		     */
+		    for (IAISRequiredPrivacySetting requiredPS : displayed
+			    .getRequiredPrivacySettings()) {
+
+			/*
+			 * Iterate through the privacy settings of the resource
+			 * group
+			 */
+			for (IRGISPrivacySetting ps : resGroup
+				.getPrivacySettings()) {
+			    if (!ps.getIdentifier().equals(
+				    requiredPS.getIdentifier())
+				    && !addedIdentifiers.contains(ps
+					    .getIdentifier())) {
+				if (!customRGIS.getPrivacySettings().contains(
+					ps)) {
+				    customRGIS.addPrivacySetting(ps);
+				}
+			    }
+			}
+		    }
+
+		    // If there are some PS to add
+		    if (!customRGIS.getPrivacySettings().isEmpty()) {
+
+			// Open the dialog
+			SelectionDialog dialog = new RequiredPrivacySettingsDialog(
+				parentShell, customRGIS);
+
+			// Get the results
+			if (dialog.open() == Window.OK
+				&& dialog.getResult().length > 0) {
+
+			    // Store them at the model
+			    for (Object object : dialog.getResult()) {
+				AISRequiredPrivacySetting rps = (AISRequiredPrivacySetting) object;
+				displayed.addRequiredPrivacySetting(rps);
+
+				AISValidatorWrapper.getInstance()
+					.validateRequiredPrivacySetting(rps,
+						true);
+			    }
+
+			    AISValidatorWrapper.getInstance()
+				    .validateRequiredResourceGroup(displayed,
+					    true);
+			    AISValidatorWrapper.getInstance()
+				    .validateServiceFeatures(model.getAis(),
+					    true);
+
+			    ServiceFeatureMasterBlock.refreshTree();
+			    // Update the view
+			    psTableViewer.refresh();
+			    psTableViewer.getTable().setRedraw(false);
+			    identifierColumn.pack();
+			    valueColumn.pack();
+			    psTableViewer.getTable().setRedraw(true);
+			    psTableViewer.getTable().redraw();
+			    model.setAISDirty(true);
+			}
+
+			// There are no PS to add to this service feature
+		    } else {
+			MessageDialog
+				.openInformation(parentShell,
+					"No Privacy Settings to add",
+					"You already added all Privacy Settings of this Resource Group");
+		    }
+		} else {
+		    /*
+		     * The Resource group wasn't found at the server and no
+		     * error happen while downloading them, show the
+		     * corresponding message
+		     */
+		    if (!error) {
+			MessageDialog
+				.openInformation(
+					parentShell,
+					"Unknown Resource Group",
+					"This Resource Group was not found at the Resource Group server.\n"
+						+ "Therefore you can not add a Privacy Setting");
+		    }
+		}
+	    }
+	}
     }
 }
