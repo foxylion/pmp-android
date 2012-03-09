dba134a9973d680566bb24fb19c009c3c3671c45
Frieder Schueler <frieder.schueler@gmail.com>
2012-03-09 22:18:16 +0100

    InfoApp RGs implementation

---
+++ pmp-android/Resources/RSSFeedResourceGroup/src/de/unistuttgart/ipvs/pmp/resourcegroups/rssfeed/RSSFeedResource.java
@@ -0,0 +1,14 @@
+package de.unistuttgart.ipvs.pmp.resourcegroups.rssfeed;
+
+import android.os.IBinder;
+import de.unistuttgart.ipvs.pmp.resource.Resource;
+
+public class RSSFeedResource extends Resource {
+
+	@Override
+	public IBinder getAndroidInterface(String appIdentifier) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
