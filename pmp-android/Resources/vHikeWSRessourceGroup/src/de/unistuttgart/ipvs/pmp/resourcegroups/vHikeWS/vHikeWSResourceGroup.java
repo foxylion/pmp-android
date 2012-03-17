efec6e89c46e2a62f6233d8e1be8470f077fc076
Alexander Wassiljew <alexanderwassiljew@googlemail.com>
2012-03-17 20:39:02 +0100

    vHike-PMP refs #439

---
+++ pmp-android/Resources/vHikeWSRessourceGroup/src/de/unistuttgart/ipvs/pmp/resourcegroups/vHikeWS/vHikeWSResourceGroup.java
@@ -0,0 +1,25 @@
+package de.unistuttgart.ipvs.pmp.resourcegroups.vHikeWS;
+
+import de.unistuttgart.ipvs.pmp.resource.IPMPConnectionInterface;
+import de.unistuttgart.ipvs.pmp.resource.ResourceGroup;
+import de.unistuttgart.ipvs.pmp.resource.privacysetting.library.BooleanPrivacySetting;
+import de.unistuttgart.ipvs.pmp.resourcegroups.vHikeWS.resource.vHikeWebserviceResource;
+
+public class vHikeWSResourceGroup extends ResourceGroup {
+
+	public static final String PACKAGE_NAME = "de.unistuttgart.ipvs.pmp.resourcegroups.vHikeWS";
+
+	public static final String R_vHIKE_WEBSERVICE= "vHikeWebserviceResource";
+
+	public static final String PS_USE_vHIKE_WEBSERVICE = "usevHikeWS";
+
+	public vHikeWSResourceGroup(IPMPConnectionInterface pmpci) {
+		super(PACKAGE_NAME, pmpci);
+		
+		
+		registerResource(R_vHIKE_WEBSERVICE, new vHikeWebserviceResource());
+		
+		registerPrivacySetting(PS_USE_vHIKE_WEBSERVICE, new BooleanPrivacySetting());
+	}
+
+}
