fc0f6ea559d078d4f9e7fb2c5d9b17d62fbb1897
Anton Makarov <anton.makarov.swt@googlemail.com>
2012-03-25 22:45:28 +0200

    InfoApp RSSFeed for Prasi implemented refs 620

--- pmp-android/Apps/InfoApp/src/de/unistuttgart/ipvs/pmp/apps/infoapp/SettingsActivity.java
+++ pmp-android/Apps/InfoApp/src/de/unistuttgart/ipvs/pmp/apps/infoapp/SettingsActivity.java
@@ -1,5 +1,71 @@
 package de.unistuttgart.ipvs.pmp.apps.infoapp;
 
-public class SettingsActivity {
+import android.app.AlertDialog;
+import android.app.ListActivity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class SettingsActivity extends ListActivity {
     
+    final Intent mResultIntent = new Intent();
+    int mCurrentPanel;
+    SharedPreferences mSettings;
+    
+    
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        final String[] panels = getIntent().getStringArrayExtra("panels");
+        final CharSequence[] asd = panels;
+        String[] options = new String[] { "Set starting Panel" };
+        mSettings = getSharedPreferences("settings", 0);
+        
+        setListAdapter(new ArrayAdapter<String>(this, R.layout.settings, options));
+        
+        ListView lv = getListView();
+        lv.setTextFilterEnabled(true);
+        
+        lv.setOnItemClickListener(new OnItemClickListener() {
+            
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                createDialog(asd);
+            }
+        });
+        
+    }
+    
+    
+    private void createDialog(final CharSequence[] items) {
+        
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Pick a color");
+        builder.setSingleChoiceItems(items, -1, new DialogInterface.OnClickListener() {
+            
+            public void onClick(DialogInterface dialog, int item) {
+                mCurrentPanel = item;
+            }
+        });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+    
+    
+    private void setSettings() {
+        SharedPreferences.Editor editor = mSettings.edit();
+        editor.putInt("panel", mCurrentPanel);
+        editor.commit();
+    }
+    
+    
+    public void onStop() {
+        super.onStop();
+        setSettings();
+        setResult(1, mResultIntent.putExtra("panelNumber", mCurrentPanel));
+    }
 }
