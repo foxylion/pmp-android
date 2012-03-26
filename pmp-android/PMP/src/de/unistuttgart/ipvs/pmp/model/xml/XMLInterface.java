7b6e12d0c0e2ad82544bfbe7a9f901c3c39dd8a7
Tobias Kuhn <kuhnts@studi.informatik.uni-stuttgart.de>
2012-03-26 20:40:44 +0200

    improves performance for importing presetsets refs #752

--- pmp-android/PMP/src/de/unistuttgart/ipvs/pmp/model/xml/XMLInterface.java
+++ pmp-android/PMP/src/de/unistuttgart/ipvs/pmp/model/xml/XMLInterface.java
@@ -13,6 +13,7 @@ import de.unistuttgart.ipvs.pmp.model.element.preset.IPreset;
 import de.unistuttgart.ipvs.pmp.model.element.privacysetting.IPrivacySetting;
 import de.unistuttgart.ipvs.pmp.model.element.resourcegroup.IResourceGroup;
 import de.unistuttgart.ipvs.pmp.model.exception.InvalidConditionException;
+import de.unistuttgart.ipvs.pmp.model.ipc.IPCProvider;
 import de.unistuttgart.ipvs.pmp.resource.privacysetting.PrivacySettingValueException;
 import de.unistuttgart.ipvs.pmp.xmlutil.presetset.IPresetAssignedApp;
 import de.unistuttgart.ipvs.pmp.xmlutil.presetset.IPresetAssignedPrivacySetting;
@@ -80,83 +81,93 @@ public class XMLInterface implements IXMLInterface {
         
         IModel m = Model.getInstance();
         
-        // each preset
-        for (de.unistuttgart.ipvs.pmp.xmlutil.presetset.IPreset preset : presets) {
-            IModelElement creator = m.getApp(preset.getCreator());
-            if (creator == null) {
-                creator = m.getResourceGroup(preset.getCreator());
-            }
-            IPreset existing = m.getPreset(creator, preset.getIdentifier());
+        IPCProvider.getInstance().startUpdate();
+        try {
             
-            // add one to the model according to semantics
-            IPreset toWrite = null;
-            
-            if (existing != null) {
-                // already exists
+            // each preset
+            for (de.unistuttgart.ipvs.pmp.xmlutil.presetset.IPreset preset : presets) {
+                IModelElement creator = m.getApp(preset.getCreator());
+                if (creator == null) {
+                    creator = m.getResourceGroup(preset.getCreator());
+                }
+                IPreset existing = m.getPreset(creator, preset.getIdentifier());
                 
-                if (override) {
-                    m.removePreset(existing.getCreator(), existing.getLocalIdentifier());
-                    toWrite = m.addPreset(creator, preset.getIdentifier(), preset.getName(), preset.getDescription());
-                } else {
-                    toWrite = m.addUserPreset(preset.getName(), preset.getDescription());
-                }
+                // add one to the model according to semantics
+                IPreset toWrite = null;
                 
-            } else {
-                // not yet, simply add
-                
-                toWrite = m.addPreset(creator, preset.getIdentifier(), preset.getName(), preset.getDescription());
-            }
-            
-            // each app
-            for (IPresetAssignedApp paa : preset.getAssignedApps()) {
-                IApp app = m.getApp(paa.getIdentifier());
-                if (app == null) {
-                    throw new InvalidPresetSetException("App not found: " + paa.getIdentifier());
-                }
-                toWrite.assignApp(app);
-            }
-            
-            // each ps
-            for (IPresetAssignedPrivacySetting pasp : preset.getAssignedPrivacySettings()) {
-                IResourceGroup rg = m.getResourceGroup(pasp.getRgIdentifier());
-                if (rg == null) {
-                    throw new InvalidPresetSetException("RG not found: " + pasp.getRgIdentifier());
-                }
-                IPrivacySetting ps = rg.getPrivacySetting(pasp.getPsIdentifier());
-                if (ps == null) {
-                    throw new InvalidPresetSetException("PS not found: " + pasp.getPsIdentifier() + " in "
-                            + pasp.getRgIdentifier());
-                }
-                try {
-                    toWrite.assignPrivacySetting(ps, pasp.getValue());
-                } catch (PrivacySettingValueException psve) {
-                    throw new InvalidPresetSetException("PS value was invalid: " + pasp.getValue(), psve);
-                }
-                
-                // each ca
-                for (IPresetPSContext ppsc : pasp.getContexts()) {
+                if (existing != null) {
+                    // already exists
                     
-                    IContext c = null;
-                    for (IContext c2 : m.getContexts()) {
-                        if (c2.getIdentifier().equals(ppsc.getType())) {
-                            c = c2;
+                    if (override) {
+                        m.removePreset(existing.getCreator(), existing.getLocalIdentifier());
+                        toWrite = m.addPreset(creator, preset.getIdentifier(), preset.getName(),
+                                preset.getDescription());
+                    } else {
+                        toWrite = m.addUserPreset(preset.getName(), preset.getDescription());
+                    }
+                    
+                } else {
+                    // not yet, simply add
+                    
+                    toWrite = m.addPreset(creator, preset.getIdentifier(), preset.getName(), preset.getDescription());
+                }
+                
+                // each app
+                for (IPresetAssignedApp paa : preset.getAssignedApps()) {
+                    IApp app = m.getApp(paa.getIdentifier());
+                    if (app == null) {
+                        throw new InvalidPresetSetException("App not found: " + paa.getIdentifier());
+                    }
+                    toWrite.assignApp(app);
+                }
+                
+                // each ps
+                for (IPresetAssignedPrivacySetting pasp : preset.getAssignedPrivacySettings()) {
+                    IResourceGroup rg = m.getResourceGroup(pasp.getRgIdentifier());
+                    if (rg == null) {
+                        throw new InvalidPresetSetException("RG not found: " + pasp.getRgIdentifier());
+                    }
+                    IPrivacySetting ps = rg.getPrivacySetting(pasp.getPsIdentifier());
+                    if (ps == null) {
+                        throw new InvalidPresetSetException("PS not found: " + pasp.getPsIdentifier() + " in "
+                                + pasp.getRgIdentifier());
+                    }
+                    try {
+                        toWrite.assignPrivacySetting(ps, pasp.getValue());
+                    } catch (PrivacySettingValueException psve) {
+                        throw new InvalidPresetSetException("PS value was invalid: " + pasp.getValue(), psve);
+                    }
+                    
+                    // each ca
+                    for (IPresetPSContext ppsc : pasp.getContexts()) {
+                        
+                        IContext c = null;
+                        for (IContext c2 : m.getContexts()) {
+                            if (c2.getIdentifier().equals(ppsc.getType())) {
+                                c = c2;
+                                break;
+                            }
+                        }
+                        if (c == null) {
+                            throw new InvalidPresetSetException("Context not found: " + ppsc.getType());
+                        }
+                        
+                        try {
+                            toWrite.assignContextAnnotation(ps, c, ppsc.getCondition(), ppsc.getOverrideValue());
+                        } catch (InvalidConditionException e) {
+                            throw new InvalidPresetSetException(
+                                    "Context condition was invalid: " + ppsc.getCondition(), e);
+                        } catch (PrivacySettingValueException e) {
+                            throw new InvalidPresetSetException("Context PS value was invalid: "
+                                    + ppsc.getOverrideValue(), e);
                         }
                     }
-                    if (c == null) {
-                        throw new InvalidPresetSetException("Context not found: " + ppsc.getType());
-                    }
-                    
-                    try {
-                        toWrite.assignContextAnnotation(ps, c, ppsc.getCondition(), ppsc.getOverrideValue());
-                    } catch (InvalidConditionException e) {
-                        throw new InvalidPresetSetException("Context condition was invalid: " + ppsc.getCondition(), e);
-                    } catch (PrivacySettingValueException e) {
-                        throw new InvalidPresetSetException("Context PS value was invalid: " + ppsc.getOverrideValue(),
-                                e);
-                    }
                 }
+                
             }
             
+        } finally {
+            IPCProvider.getInstance().endUpdate();
         }
         
     }
