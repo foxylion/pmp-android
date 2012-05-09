4fa221eb4afb8f8bca36d617239d24032abbbc9d
Alexander Wassiljew <alexanderwassiljew@googlemail.com>
2012-05-09 12:30:09 +0200

    vHike WS RG refs 800

--- pmp-android/Resources/vHikeWSRessourceGroup/src/de/unistuttgart/ipvs/pmp/resourcegroups/vHikeWS/resource/vHikeWebserviceResource.java
+++ pmp-android/Resources/vHikeWSRessourceGroup/src/de/unistuttgart/ipvs/pmp/resourcegroups/vHikeWS/resource/vHikeWebserviceResource.java
@@ -533,4 +533,55 @@ public class vHikeWebserviceResource extends Resource {
         return ret;
     }
     
+    
+    @Override
+    public IBinder getMockedAndroidInterface(String appIdentifier) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+    
+    @Override
+    public IBinder getCloakedAndroidInterface(String appIdentifier) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+    
+    public String disableObservation(String sessionID, String user_id) {
+        listToParse.clear();
+        listToParse.add(new ParamObject("sid", sessionID, false));
+        listToParse.add(new ParamObject("user_id", user_id, false));
+        String ret = "";
+        try {
+            ret = JSonRequestProvider.doRequest(listToParse, "activate_observation.php").toString();
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (NullPointerException e) {
+        }
+        
+        return ret;
+    }
+    
+    
+    public String enableObservation(String sessionID, String user_id) {
+        
+        listToParse.clear();
+        listToParse.add(new ParamObject("sid", sessionID, false));
+        listToParse.add(new ParamObject("user_id", user_id, false));
+        String ret = "";
+        try {
+            ret = JSonRequestProvider.doRequest(listToParse, "deactivate_observation.php").toString();
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (NullPointerException e) {
+        }
+        
+        return ret;
+    }
+    
 }
