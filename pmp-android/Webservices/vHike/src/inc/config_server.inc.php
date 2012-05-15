c89012200460bdac1d4d121e41f317600e2adeb3
Alexander Wassiljew <alexanderwassiljew@googlemail.com>
2012-05-15 13:56:18 +0200

    vHike fixes refs #850

--- pmp-android/Webservices/vHike/src/inc/config_server.inc.php
+++ pmp-android/Webservices/vHike/src/inc/config_server.inc.php
@@ -2,19 +2,17 @@
 if (!defined("INCLUDE")) {
     exit;
 }
+error_reporting(NULL);
 //error_reporting(E_ALL);
 //error_reporting(E_ERROR | E_PARSE);
-error_reporting(NULL);
 
 define("DEBUG",     true);
 define("FORMAT_JSON", true);
-
-define("BASE_URL", "vhike.no-ip.org");
-define("ADMIN_EMAIL", "admin@vhike.no-ip.org");
 
 define("DB_HOST",   "localhost");
 define("DB_USER",   "pmp_vhike");
 define("DB_PASSW",  "nC64DQvY79XXPmZz");
 define("DB_NAME",   "pmp_vhike");
 define("DB_PREFIX", "dev");
+
 ?>
