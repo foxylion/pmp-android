481168647795edb543965a73123b6576930cb79d
Hai Dang Huynh <kannextic@gmail.com>
2012-05-20 01:22:31 +0200

    resolve conflicts for vHike's AndroidManifest.xml refs #802

--- pmp-android/Apps/vHike/src/de/unistuttgart/ipvs/pmp/apps/vhike/gui/MyTripActivity.java
+++ pmp-android/Apps/vHike/src/de/unistuttgart/ipvs/pmp/apps/vhike/gui/MyTripActivity.java
@@ -3,13 +3,7 @@ package de.unistuttgart.ipvs.pmp.apps.vhike.gui;
 import android.app.ListActivity;
 import android.os.Bundle;
 import android.widget.ListView;
-import de.unistuttgart.ipvs.pmp.Log;
-import de.unistuttgart.ipvs.pmp.apps.vhike.Constants;
-import de.unistuttgart.ipvs.pmp.apps.vhike.ctrl.Controller;
-import de.unistuttgart.ipvs.pmp.apps.vhike.ctrl.vHikeService;
 import de.unistuttgart.ipvs.pmp.apps.vhike.gui.adapter.MyTripAdapter;
-import de.unistuttgart.ipvs.pmp.apps.vhike.gui.maps.ViewModel;
-import de.unistuttgart.ipvs.pmp.apps.vhike.model.Model;
 
 /**
  * 
@@ -37,13 +31,13 @@ public class MyTripActivity extends ListActivity {
     public void onResume() {
         super.onResume();
         
-        Controller ctrl = new Controller(ViewModel.getInstance().getvHikeRG());
-        
-        if (vHikeService.isServiceFeatureEnabled(Constants.SF_HIDE_CONTACT_INFO)) {
-            ctrl.enableAnonymity(Model.getInstance().getSid());
-        } else {
-            ctrl.disableAnonymity(Model.getInstance().getSid());
-        }
-        Log.i(this, "");
+        //        Controller ctrl = new Controller(ViewModel.getInstance().getvHikeRG());
+        //        
+        //        if (vHikeService.isServiceFeatureEnabled(Constants.SF_HIDE_CONTACT_INFO)) {
+        //            ctrl.enableAnonymity(Model.getInstance().getSid());
+        //        } else {
+        //            ctrl.disableAnonymity(Model.getInstance().getSid());
+        //        }
+        //        Log.i(this, "");
     }
 }
