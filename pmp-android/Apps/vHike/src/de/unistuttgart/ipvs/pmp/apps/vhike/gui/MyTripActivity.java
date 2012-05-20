6e37e5770923b829ea82828a31ecf63509847446
Hai Dang Huynh <kannextic@gmail.com>
2012-05-20 03:17:28 +0200

    vHike: finished GUI and Adapter for "my trips" activity refs #802

--- pmp-android/Apps/vHike/src/de/unistuttgart/ipvs/pmp/apps/vhike/gui/MyTripActivity.java
+++ pmp-android/Apps/vHike/src/de/unistuttgart/ipvs/pmp/apps/vhike/gui/MyTripActivity.java
@@ -1,9 +1,16 @@
 package de.unistuttgart.ipvs.pmp.apps.vhike.gui;
 
+import java.util.ArrayList;
+
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
+import android.widget.Toast;
 import de.unistuttgart.ipvs.pmp.apps.vhike.gui.adapter.MyTripAdapter;
+import de.unistuttgart.ipvs.pmp.apps.vhike.model.CompactTrip;
 
 /**
  * 
@@ -16,14 +23,32 @@ public class MyTripActivity extends ListActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
-        String[] names = new String[] { "Linux", "Windows7", "Eclipse", "Suse", "Ubuntu", "Solaris", "Android",
-                "iPhone", "Linux", "Windows7", "Eclipse" };
+        ArrayList<CompactTrip> t = new ArrayList<CompactTrip>(10);
+        t.add(new CompactTrip(1, "Berlin", 1000000000, 2, 3, 1));
+        t.add(new CompactTrip(2, "Stuttgart", 1000000000, 0, 3, 1));
+        t.add(new CompactTrip(3, "München", 1000000000, 2, 0, 1));
+        t.add(new CompactTrip(4, "Köln", 1000000000, 2, 3, 0));
+        t.add(new CompactTrip(5, "Düsseldorf", 1000000000, 2, 3, 1));
+        t.add(new CompactTrip(6, "Hamburg", 1000000000, 0, 0, 0));
+        t.add(new CompactTrip(7, "Text", 1000000000, 2, 3, 1));
+        t.add(new CompactTrip(8, "test", 1000000000, 2, 3, 1));
+        t.add(new CompactTrip(9, "Hallihallo", 1000000000, 2, 3, 1));
+        t.add(new CompactTrip(10, "alohodsnheif", 1000000000, 2, 3, 1));
+        
+        ListView listView = getListView();
+        listView.setAdapter(new MyTripAdapter(this, t));
+        listView.setOnItemClickListener(new OnItemClickListener() {
+            
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), "Click ListItem Number " + position, Toast.LENGTH_LONG).show();
+            }
+        });
+        
         //        View header = getLayoutInflater().inflate(R.layout.header, null);
         //        View footer = getLayoutInflater().inflate(R.layout.footer, null);
-        ListView listView = getListView();
         //        listView.addHeaderView(header);
         //        listView.addFooterView(footer);
-        listView.setAdapter(new MyTripAdapter(this, names));
     }
     
     
--- pmp-android/Apps/vHike/src/de/unistuttgart/ipvs/pmp/apps/vhike/gui/adapter/MyTripAdapter.java
+++ pmp-android/Apps/vHike/src/de/unistuttgart/ipvs/pmp/apps/vhike/gui/adapter/MyTripAdapter.java
@@ -1,7 +1,7 @@
 package de.unistuttgart.ipvs.pmp.apps.vhike.gui.adapter;
 
-import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
@@ -12,32 +12,59 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 import de.unistuttgart.ipvs.pmp.apps.vhike.R;
+import de.unistuttgart.ipvs.pmp.apps.vhike.model.CompactTrip;
 
-public class MyTripAdapter extends ArrayAdapter<String> {
+public class MyTripAdapter extends ArrayAdapter<CompactTrip> {
     
     private Context context;
-    private String[] values;
+    private ArrayList<CompactTrip> trips;
     
     
-    public MyTripAdapter(Context context, String[] values) {
-        super(context, R.layout.layout_my_trip_list_item, values);
+    public MyTripAdapter(Context context, ArrayList<CompactTrip> trips) {
+        super(context, R.layout.layout_my_trip_list_item, trips);
         this.context = context;
-        this.values = values;
+        this.trips = trips;
     }
     
     
     public View getView(int position, View convertView, ViewGroup parent) {
+        
+        // Inflate layout
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View itemView = inflater.inflate(R.layout.layout_my_trip_list_item, parent, false);
+        
+        // set views
         TextView departure = (TextView) itemView.findViewById(R.id.myTrip_destination);
-        departure.setText(values[position]);
+        departure.setText(trips.get(position).destination);
+        
         TextView date = (TextView) itemView.findViewById(R.id.myTrip_date);
-        TextView time = (TextView) itemView.findViewById(R.id.myTrip_time);
         Calendar c = GregorianCalendar.getInstance();
-        DateFormat df = new SimpleDateFormat("dd/MM/yy");
-        date.setText(df.format(c.getTime()));
-        time.setText(SimpleDateFormat.getTimeInstance(SimpleDateFormat.SHORT).format(c.getTime()));
+        date.setText(SimpleDateFormat.getDateTimeInstance(SimpleDateFormat.SHORT, SimpleDateFormat.SHORT).format(
+                c.getTime()));
+        
+        int i = 0;
+        
+        TextView passenger = (TextView) itemView.findViewById(R.id.myTrip_passengers);
+        i = trips.get(position).numberOfPassengers;
+        passenger.setText(String.valueOf(i));
+        if (i > 0) {
+            passenger.setBackgroundResource(R.drawable.bg_round_blue);
+        }
+        
+        TextView request = (TextView) itemView.findViewById(R.id.myTrip_requests);
+        i = trips.get(position).numberOfOffers;
+        request.setText(String.valueOf(i));
+        if (i > 0) {
+            request.setBackgroundResource(R.drawable.bg_round_green);
+        }
+        
+        TextView message = (TextView) itemView.findViewById(R.id.myTrip_messages);
+        i = trips.get(position).numberOfNewMessages;
+        message.setText(String.valueOf(i));
+        if (i > 0) {
+            message.setBackgroundResource(R.drawable.bg_round_red);
+        }
+        
         return itemView;
     }
-    
 }
