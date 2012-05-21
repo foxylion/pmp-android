177d3b183d5bc4544d6e11e040d3852205c5d22a
andres <nguyen.andres@gmail.com>
2012-05-21 11:52:25 +0200

    contactrg isSFEnabled, requestSF refs #840

--- pmp-android/Webservices/vHike/src/inc/class/offer.class.php
+++ pmp-android/Webservices/vHike/src/inc/class/offer.class.php
@@ -72,6 +72,27 @@ class Offer {
 		return $offer;
 	}
 
+	public static function getOfferReply($tripId, $uid) {
+		$db = Database::getInstance();
+		$row = $db->fetch($db->query("SELECT * FROM dev_offer WHERE trip=$tripId AND recipient=$uid"));
+		
+		if ($db->getAffectedRows() <= 0) {
+            return null;
+        }
+		
+		$offer = new Offer();
+		
+		$offer->id = $row["id"];
+		$offer->trip = $row["trip"];
+		$offer->query = $row["query"];
+		$offer->status = $row["status"];
+		$offer->sender = $row["sender"];
+		$offer->recipient = $row["recipient"];
+		$offer->message = $row["message"];
+		
+		return $offer;
+	}
+	
 	/**
 	 * Load's all messages that have been send to a given user's queries.
 	 * E.g. if a user has opened 3 queries, this will return all offers
@@ -210,6 +231,8 @@ class Offer {
 
 		return $count["count"] > 0;
 	}
+
+
 
 	/**
 	 * Returns the offer id
