23cdec7c008d07310c030d385128475ed8488c88
Alexander Wassiljew <alexanderwassiljew@googlemail.com>
2012-05-30 11:53:16 +0200

    Comments added refs #866

--- pmp-android/Resources/BluetoothResourceGroup/src/de/unistuttgart/ipvs/pmp/resourcegroups/bluetooth/objects/DeviceArray.java
+++ pmp-android/Resources/BluetoothResourceGroup/src/de/unistuttgart/ipvs/pmp/resourcegroups/bluetooth/objects/DeviceArray.java
@@ -8,16 +8,29 @@ import java.util.Set;
 import android.bluetooth.BluetoothDevice;
 import android.os.Parcel;
 import android.os.Parcelable;
-
+/**
+ * Hold the devices in an Array
+ * @author Alexander Wassiljew
+ *
+ */
 public class DeviceArray implements Serializable {
-
+	/**
+	 * List of Devices
+	 */
 	public List<String> devices;
-	
+	/**
+	 * Constructor
+	 * @param devices
+	 */
 	public DeviceArray(List<String> devices) {
 		this.devices = devices;
 	}
 
-	
+	/**
+	 * Returns the devices 
+	 * 
+	 * @return devices
+	 */
 	public List<String> getDevices(){
 		return devices;
 	}
